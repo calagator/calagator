3a3c462324c8f7c271eb1c331d5b792ff9cfbf16
igal.koshevoy <igal.koshevoy@54c6411d-1744-0410-a9eb-b99409f43359>
2008-01-20 01:33:31 +0000

    Now able to parse hcal events
    git-svn-id: https://calagator.googlecode.com/svn/trunk@21 54c6411d-1744-0410-a9eb-b99409f43359

--- app/models/source.rb
+++ app/models/source.rb
@@ -12,8 +12,25 @@
 #  updated_at  :datetime        
 #
 
+require 'mofo'
+require 'open-uri'
+
 class Source < ActiveRecord::Base
-  def parse
-    # TODO like parse this source and stuff
+  # Returns an Array of Event objects that were imported into this queue.
+  def parse(opts={})
+    send "parse_#{format_type.downcase}", opts
+  end
+
+  # Returns content for a URL. Easier to stub.
+  def read_url(url)
+    open(url){|h| h.read}
+  end
+
+  def parse_hcal(opts)
+    returning([]) do |events|
+      for result in [hCalendar.find(:text => read_url(url))].flatten
+        events << Event.new(:title => result.summary, :description => result.description, :start_time => result.dtstart, :url => result.url)
+      end
+    end
   end
 end
--- spec/models/source_spec.rb
+++ spec/models/source_spec.rb
@@ -6,62 +6,30 @@ describe Source do
   end
 
   it "should parse hcal" do
-    hcal_content = <<-HERE# {{{
-<-- ADD CLASS="VEVENT" TO PAGE BODY-->
-
-
-<body  class ="vevent" vlink="#666666" text="#ffffff" marginwidth="0" marginheight="0" link="#cccccc" bgcolor="#000000" alink="#999999" onload="preloadImages();" topmargin="0" leftmargin="0">
-
- 
-
-<...>
-
-
-<-- ADD CLASS ="SUMMARY"-->
-
-<td id="contentText" width="50%" valign="top" align="left">
-<h2>MEETING INFORMATION :</h2>
-
-<p>
-<br/>
-<div class="summary">Portland Final Cut User Group now meets on the first Tuesday of every month from 6:30-8:30 PM.</div>
-<br/>
-<br/>
-The location is:
-<br/>
-<font face="Arial, Helvetica, sans-serif">
-<-- ADD CLASS ="LOCATION"-->
-<span class="location">PCC/Cascade Campus
-<br/>
-705 N. Killingsworth St. (x Albina)
-<br/>
-Moriarty Arts and Humanities Building, Room 104.</span>
-<br/>
-<-- ADD CLASS ="URL"-->
-<span class="url">Link:
-<a target="_blank" href="http://www.pcc.edu/about/locations/cascade/">www.pcc.edu/about/locations/cascade/ </a></span>
-</font>
-<br/>
-<br/>
-</p>
- 
-<...>
-
-<td id="contentText" width="50%">
-<-- ADD CLASS ="DTSTART"-->
-<h2 class="dtstart">Tuesday December 04, 2007 6:15pm AGENDA:</h2>
-<table>
-<tbody>
-<tr>
-<td height="20">6:15 - 6:30</td>
+    hcal_content = <<-HERE
+<div class="vevent">
+  <a href="http://www.cubespacepdx.com" class="url">
+  <abbr title="20080119" class="dtstart">January 19th</abbr>,
+  <abbr title="20080120" class="dtend"> 2008</abbr>
+  <span class="summary">Calendar event</span>– at
+  <span class="location">Cubespace, 622 SE Grand Ave. Portland, OR 97214</span></a>
+  <div class="description">Check it out!</div>
+</div>
     HERE
-# }}}
+    hcal_source = Source.new(:title => "Calendar event feed", :url => "http://mysample.hcal/", :format_type => "hcal")
+    hcal_source.should_receive(:read_url).and_return(hcal_content)
 
-    final_cut_hcal = Source.new(:title => "Final Cut User Group", :url => "FIXME", :format_type => "hcal")
-
-    final_cut_hcal.parse.should == [
-      Event.new(:title => nil, :description => nil, :start_time => nil, :url => nil, :venue => nil) # TODO write what this is expected to create
-    ]
+    events = hcal_source.parse(:preview => true)
+    events.size.should == 1
+    for key, value in {
+      :title => "Calendar event",
+      :description => "Check it out!",
+      :start_time => Time.parse("2008-1-19"),
+      :url => "http://www.cubespacepdx.com",
+      :venue => nil, # TODO what should venue instance be?
+    }
+      events.first[key].should == value
+    end
 
   end
 end
