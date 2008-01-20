972b4e0f19c80eab1d0fecb3ea8f8beca71bbadd
igal.koshevoy <igal.koshevoy@54c6411d-1744-0410-a9eb-b99409f43359>
2008-01-20 17:07:17 +0000

    Organized columns
    git-svn-id: https://calagator.googlecode.com/svn/trunk@34 54c6411d-1744-0410-a9eb-b99409f43359

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   active_scaffold :event do |config|
     config.list.columns = [:title, :description, :start_time, :venue, :url]
+    config.columns = [:title, :description, :start_time, :venue, :url, :created_at, :updated_at]
   end
 end
--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,5 +1,6 @@
 class VenuesController < ApplicationController
   active_scaffold :venues do |config|
-    config.list.columns = [:title, :description, :address, :url]
+    config.list.columns = [:title, :address, :url]
+    config.columns = [:title, :address, :url, :description, :created_at, :updated_at]
   end
 end
