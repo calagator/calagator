cc03cf12b9b3fa0ca84de3fbed282988aee27f3c
igal.koshevoy <igal.koshevoy@54c6411d-1744-0410-a9eb-b99409f43359>
2008-01-21 00:01:15 +0000

    Added basic webadmin console
    git-svn-id: https://calagator.googlecode.com/svn/trunk@38 54c6411d-1744-0410-a9eb-b99409f43359

--- script/webadmin.rb
+++ script/webadmin.rb
@@ -1,12 +1,87 @@
 #!/usr/bin/env ruby
 
+# This is a simple, stand-alone web application that provides a way to start, stop, restart, status check, and deploy the application. Execute this program with a "-h" flag for usage instructions.
+
 require 'rubygems'
 require 'ramaze'
+require 'optparse'
+
+OWN_FILE = File.expand_path(__FILE__)
+PID_FILE = OWN_FILE+".pid"
+SOCK_FILE = OWN_FILE+".sock"
+RAILS_ENV = ENV['CALAGATOR_DEVELOPMENT'] ? "development" : "production"
+RAILS_ROOT = File.expand_path(File.dirname(File.dirname(__FILE__)))
+
+# Are we running in production mode?
+def production?; RAILS_ENV == "production"; end
+
+# Is process running? Returns PID if true, false otherwise.
+def running?
+  if File.exist?(PID_FILE)
+    pid = File.read(PID_FILE).to_i
+    begin
+      # Running
+      Process.kill(0, pid)
+      return pid
+    rescue Errno::ESRCH
+      # Removed stale PID file
+      File.unlink(PID_FILE)
+      return false
+    end
+  else
+    # Not running
+    return false
+  end
+end
+
+options = {}
+opts = OptionParser.new do |opts|
+  opts.banner = "Usage: example.rb [options]"
+  opts.on("-d", "--daemonize", "Daemonize script") do |v|
+    options[:daemonize] = v
+  end
+  opts.on("-a", "--attach", "Attach to script") do |v|
+    options[:attach] = v
+  end
+  opts.on("-h", "--help", "Show this help")
+  args = opts.parse!(ARGV)
+
+  if args.include? "start"
+    puts "Starting"
+    exec "dtach -n #{SOCK_FILE} #{OWN_FILE} --daemonize"
+  elsif args.include? "stop"
+    if pid = running?
+      Process.kill("TERM", pid)
+      puts "Stopped PID #{pid}"
+      exit 0
+    else
+      puts "Not running"
+      exit 1
+    end
+  elsif args.include? "status"
+    if pid = running?
+      puts "Running at PID #{pid}"
+      exit 0
+    else
+      puts "Not running"
+      exit 7
+    end
+  elsif args.include? "attach" || options[:attach]
+    exec "dtach -a #{SOCK_FILE}"
+  end
+  if !options[:daemonize]
+    puts opts.help
+    puts "ERROR: Unknown arguments #{args.size > 0 ? (' -- '+args.inspect) : ''}"
+    exit 1
+  end
+end
 
 class MainController < Ramaze::Controller
   engine :Haml
 
-  RAILS_ROOT = File.expand_path(File.dirname(File.dirname(__FILE__)))
+  if production?
+    def error() 'An error occurred. Contact one of the developers.' end
+  end
 
   def index
     case request["action"]
@@ -56,5 +131,12 @@ class MainController < Ramaze::Controller
   end
 end
 
-# TODO accept all ips?
-Ramaze.start :adapter => :mongrel, :host => "*", :port => 7000
+puts "Starting#{production? ? " production" : ""} server at PID: #{Process.pid}"
+File.open(PID_FILE, "w+"){|h| h.write(Process.pid)}
+
+Ramaze.start({ 
+  :adapter => :mongrel, 
+  :host => production? ? 'localhost' : '*', 
+  :port => 20019,
+  :sourcereload => production? ? false : true,
+})
