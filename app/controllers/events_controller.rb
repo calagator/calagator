ee884ce780d1deb6f1e4a662f9d90afd27ad0af7
anselm <anselm@54c6411d-1744-0410-a9eb-b99409f43359>
2008-02-16 19:47:22 +0000

    show inline link and events show overrides active scaffold
    git-svn-id: https://calagator.googlecode.com/svn/trunk@102 54c6411d-1744-0410-a9eb-b99409f43359

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,5 +2,6 @@ class EventsController < ApplicationController
   active_scaffold :event do |config|
     config.list.columns = [:url, :title, :description, :to_hcal, :start_time, :venue]
     config.columns = [:url, :title, :description, :start_time, :venue, :created_at, :updated_at]
+    config.show.link.inline = false
   end
 end
