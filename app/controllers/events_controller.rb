f8e2165e4e440a7d31a51599811a919ce49fffa4
igal.koshevoy <igal.koshevoy@54c6411d-1744-0410-a9eb-b99409f43359>
2008-02-16 20:38:02 +0000

    Changed event list sorting to start date descending
    git-svn-id: https://calagator.googlecode.com/svn/trunk@106 54c6411d-1744-0410-a9eb-b99409f43359

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   active_scaffold :event do |config|
+    config.list.sorting = {:start_time => :desc}
     config.list.columns = [:url, :title, :description, :to_hcal, :start_time, :venue]
     config.columns = [:url, :title, :description, :start_time, :venue, :created_at, :updated_at]
     config.show.link.inline = false
