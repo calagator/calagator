8226421d352f20d55e658e484d28df8932b5fbd7
igal.koshevoy <igal.koshevoy@54c6411d-1744-0410-a9eb-b99409f43359>
2008-03-02 08:28:49 +0000

    Fixed Venues views because these had been left in a broken state. The list/show/edit/update cycle now works again. Moved ActiveScaffold to its own admin controller, and added views and resource actions to the VenuesController.
    git-svn-id: https://calagator.googlecode.com/svn/trunk@252 54c6411d-1744-0410-a9eb-b99409f43359

---
+++ app/controllers/admin/venues_controller.rb
@@ -0,0 +1,26 @@
+class Admin::VenuesController < ApplicationController
+  active_scaffold :venues do |config|
+    config.list.columns = [:title, :url]
+    config.show.link.inline = false
+    config.columns = [
+      :title, 
+      :url, 
+      :description, 
+
+      :address, 
+
+      :street_address,
+      :locality,
+      :region,
+      :postal_code,
+      :country,
+
+      :latitude,
+      :longitude,
+      
+      :created_at, 
+      :updated_at,
+    ]
+    config.update.link.inline = false
+  end
+end
--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,26 +1,85 @@
 class VenuesController < ApplicationController
-  active_scaffold :venues do |config|
-    config.list.columns = [:title, :url]
-    config.show.link.inline = false
-    config.columns = [
-      :title, 
-      :url, 
-      :description, 
+  # GET /venues
+  # GET /venues.xml
+  def index
+    @venues = Venue.find(:all)
 
-      :address, 
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @venues }
+    end
+  end
 
-      :street_address,
-      :locality,
-      :region,
-      :postal_code,
-      :country,
+  # GET /venues/1
+  # GET /venues/1.xml
+  def show
+    @venue = Venue.find(params[:id])
 
-      :latitude,
-      :longitude,
-      
-      :created_at, 
-      :updated_at,
-    ]
-    config.update.link.inline = false
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @venue }
+    end
+  end
+
+  # GET /venues/new
+  # GET /venues/new.xml
+  def new
+    @venue = Venue.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @venue }
+    end
+  end
+
+  # GET /venues/1/edit
+  def edit
+    @venue = Venue.find(params[:id])
+  end
+
+  # POST /venues
+  # POST /venues.xml
+  def create
+    @venue = Venue.new(params[:venue])
+
+    respond_to do |format|
+      if @venue.save
+        flash[:success] = 'Venue was successfully created.'
+        format.html { redirect_to(@venue) }
+        format.xml  { render :xml => @venue, :status => :created, :location => @venue }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @venue.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /venues/1
+  # PUT /venues/1.xml
+  def update
+    @venue = Venue.find(params[:id])
+
+    respond_to do |format|
+      if @venue.update_attributes(params[:venue])
+        flash[:success] = 'Venue was successfully updated.'
+        format.html { redirect_to(@venue) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @venue.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /venues/1
+  # DELETE /venues/1.xml
+  def destroy
+    @venue = Venue.find(params[:id])
+    @venue.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(venues_url) }
+      format.xml  { head :ok }
+    end
   end
 end
