5e6726fd4a988f2fc94be22d7301f0bf11fa1516
igal.koshevoy <igal.koshevoy@54c6411d-1744-0410-a9eb-b99409f43359>
2008-03-02 08:27:26 +0000

    Added iCalendar parser and specs.
    git-svn-id: https://calagator.googlecode.com/svn/trunk@246 54c6411d-1744-0410-a9eb-b99409f43359

---
+++ app/models/source_parser/ical.rb
@@ -0,0 +1,85 @@
+# Loads vpim from vendor/gems
+require 'vpim/icalendar'
+require 'vpim/vcard'
+
+class SourceParser # :nodoc:
+  # == SourceParser::Ical
+  #
+  # Reads iCalendar events.
+  #
+  # Example:
+  #   abstract_events = SourceParser::Ical.to_abstract_events('http://upcoming.yahoo.com/calendar/v2/event/349225')
+  #
+  # Sample sources:
+  #   http://upcoming.yahoo.com/calendar/v2/event/349225
+  #   webcal://appendix.23ae.com/calendars/AlternateHolidays.ics
+  #   http://appendix.23ae.com/calendars/AlternateHolidays.ics
+  class Ical < Base
+    label :iCalendar
+
+    # Return an Array of AbstractEvent instances extracted from an iCalendar input.
+    #
+    # Options:
+    # * :url -- URL of iCalendar data to import
+    def self.to_abstract_events(opts={})
+      content = read_url(opts[:url])
+
+      content_calendars = content.scan(/^BEGIN:VCALENDAR$.*^END:VCALENDAR$/m)
+      content_calendars.map do |content_calendar|
+        returning(AbstractEvent.new) do |event|
+          ical = Vpim::Icalendar.decode(content_calendar).first
+          component = ical.components.first
+
+          event.title = component.summary
+          event.description = component.description
+          event.start_time = component.dtstart
+          event.url = component.url
+          event.location = to_abstract_location(content_calendar, :fallback => component.location)
+          event
+        end
+      end
+    end
+
+    # Return an AbstractLocation extracted from an iCalendar input.
+    #
+    # Arguments:
+    # * value - String with iCalendar data to parse which contains a Vvenue item.
+    #
+    # Options:
+    # * :fallback - String to use as the title for the event if the +value+ doesn't contain a Vvenue.
+    def self.to_abstract_location(value, opts={})
+      a = AbstractLocation.new
+      fallback_or_nil = lambda{opts[:fallback].blank? ? nil : (a.title = opts[:fallback] && a)}
+
+      # The Vpim libary doesn't understand that Vvenue entries are just Vcards,
+      # so transform the content to trick it into treating them as such.
+      if vcard_content = value.scan(/^BEGIN:VVENUE$.*^END:VVENUE$/m).first
+        vcard_content.gsub!(/^BEGIN:VVENUE$/m, 'BEGIN:VCARD')
+        vcard_content.gsub!(/^END:VVENUE$/m, 'END:VCARD')
+
+        begin
+          # TODO What if there is more than one vcard in the vcalendar?!
+          vcards = Vpim::Vcard.decode(vcard_content)
+          raise ArgumentError, "Wrong number of vcards" if vcards.size != 1
+          vcard = vcards.first
+
+          a.title = vcard['name']
+          a.street_address = vcard['address']
+          a.locality = vcard['city']
+          a.region = vcard['region']
+          a.postal_code = vcard['postalcode']
+          a.country = vcard['country']
+
+          a.latitude, a.longitude = vcard['geo'].split(/;/).map(&:to_f)
+
+          return a
+        rescue Vpim::InvalidEncodingError, ArgumentError
+          return fallback_or_nil.call
+        end
+      else
+        return fallback_or_nil.call
+      end
+    end
+  end
+end
+
--- spec/models/source_spec.rb
+++ spec/models/source_spec.rb
@@ -1,10 +1,6 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe Source do
-  before(:each) do
-    @source = Source.new
-  end
-
+describe Source, "with hCalendar events" do
   it "should parse hcal" do
     hcal_content = read_sample('hcal_single.xml')
     hcal_source = Source.new(:title => "Calendar event feed", :url => "http://mysample.hcal/", :format_type => "hcal")
@@ -22,10 +18,10 @@ describe Source do
       events.first[key].should == value
     end
   end
-  
+
   it "should parse a page with more than one hcal item in it" do
     hcal_content = read_sample('hcal_multiple.xml')
-    
+
     hcal_source = Source.new(:title => "Calendar event feed", :url => "http://mysample.hcal/", :format_type => "hcal")
     SourceParser::Hcal.should_receive(:read_url).and_return(hcal_content)
 
@@ -35,5 +31,52 @@ describe Source do
     first[:start_time ].should == Time.parse('2008-1-19')
     second[:start_time].should == Time.parse('2008-2-2')
   end
-    
+end
+
+describe Source, "with iCalendar events" do
+  def events_from_ical_at(filename)
+    url = "http://foo.bar/"
+    content = read_sample(filename)
+    source = Source.new(:title => "Calendar event feed", :url => url, :format_type => "ical")
+    SourceParser::Ical.should_receive(:read_url).and_return(content)
+    return source.to_events
+  end
+
+  it "should parse Apple iCalendar format" do
+    events = events_from_ical_at('ical_apple.ics')
+
+    events.size.should == 1
+    event = events.first
+    event.title.should =~ /Coffee with Jason/
+    event.start_time.should == Time.parse('Mon Oct 28 14:00:00 -0800 2002')
+    event.venue.should be_nil
+  end
+
+  it "should parse basic iCalendar format" do
+    events = events_from_ical_at('ical_basic.ics')
+
+    events.size.should == 1
+    event = events.first
+    event.title.should be_blank
+    event.start_time.should == Time.parse('Wed Jan 17 00:00:00 UTC 2007')
+    event.venue.should be_nil
+  end
+
+  it "should parse Upcoming iCalendar format" do
+    events = events_from_ical_at('ical_upcoming.ics')
+
+    events.size.should == 1
+    event = events.first
+    event.title.should =~ /Ignite Portland/
+    event.start_time.should == Time.parse('Tue Feb 05 18:00:00 -0800 2008')
+    event.description.should =~ /What if you only got 20 slides/
+    event.venue.should_not be_blank
+    event.venue.title.should =~ /Bagdad Theater/
+    event.venue.locality.should =~ /Portland/
+    event.venue.country.should =~ /United States/
+    event.venue.postal_code.should =~ /97214/
+    event.venue.latitude.should == 45.5121
+    event.venue.longitude.should == -122.626
+  end
+>>>>>>> Added iCalendar parser and specs.:spec/models/source_spec.rb
 end
