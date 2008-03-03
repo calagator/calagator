5046f370b4f8b36f771c7e2368a30a8825195116
spinnerin <spinnerin@54c6411d-1744-0410-a9eb-b99409f43359>
2008-03-03 17:42:30 +0000

    Fixing an error in which items were displayed as today's and tomorrow's events on the home page.
    git-svn-id: https://calagator.googlecode.com/svn/trunk@301 54c6411d-1744-0410-a9eb-b99409f43359

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @events_today = Event.find(:all, :conditions => {:start_time => Time.today..Time.today+1.days}, :order => 'start_time ASC', :limit => 6)
-    @events_tomorrow = Event.find(:all, :conditions => {:start_time => Time.today+1.days..Time.today+2.days}, :order => 'start_time ASC', :limit => 3)
-    @events_later = Event.find(:all, :conditions => {:start_time => Time.today+2.days..(Time.today+7.days)}, :order => 'start_time ASC', :limit => 3)
+    @events_today = Event.find(:all, 
+        :conditions => { :start_time => Time.today..(Time.today + 1.days) }, :order => 'start_time ASC', :limit => 6)
+    @events_tomorrow = Event.find(:all, 
+        :conditions => { :start_time => (Time.today + 1.days)..(Time.today + 2.days) }, :order => 'start_time ASC', :limit => 3)
+    @events_later = Event.find(:all, 
+        :conditions => { :start_time => (Time.today + 2.days)..(Time.today + 7.days) }, :order => 'start_time ASC', :limit => 3)
     
     @recently_added_events = Event.find(:all, :order => 'created_at DESC', :limit => 5)
   end
