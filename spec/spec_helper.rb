57da260c8707868098737ecc1067475610dc4a8b
reidab <reidab@54c6411d-1744-0410-a9eb-b99409f43359>
2008-03-08 03:18:58 +0000

    r201@reid-powerbook:  reidab | 2008-03-07 19:18:52 -0800  - Switched the XHTML validation fragment wrapper to use XHTML 1.0 strict, as the rest of the site does.
    git-svn-id: https://calagator.googlecode.com/svn/trunk@345 54c6411d-1744-0410-a9eb-b99409f43359

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -117,11 +117,10 @@ class BeValidXhtml
   def wrap_with_xhtml_header(fragment)
     ret = <<-EOS
 <?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE html PUBLIC
-    "-//W3C//DTD XHTML 1.1 plus MathML 2.0 plus SVG 1.1//EN"
-    "http://www.w3.org/2002/04/xhtml-math-svg/xhtml-math-svg.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
   <title>Test</title>
 </head>
