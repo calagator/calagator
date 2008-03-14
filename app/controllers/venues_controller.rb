33280c83c48e121f0bf4840c09ebdf81e8f8daf2
reidab <reidab@54c6411d-1744-0410-a9eb-b99409f43359>
2008-03-14 20:19:43 +0000

    Added parens on redirect_to in the venues controller to avoid a warning from rspec.
    git-svn-id: https://calagator.googlecode.com/svn/trunk@366 54c6411d-1744-0410-a9eb-b99409f43359

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -15,7 +15,7 @@ class VenuesController < ApplicationController
   def show
     @venue = Venue.find(params[:id])
 
-    return redirect_to venue_url(@venue.duplicate_of) if @venue.duplicate_of_id
+    return redirect_to(venue_url(@venue.duplicate_of)) if @venue.duplicate_of_id
 
     respond_to do |format|
       format.html # show.html.erb
