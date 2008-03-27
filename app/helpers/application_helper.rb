c8ad8a0d71352eee6513578845964e920dcf1edc
igal.koshevoy <igal.koshevoy@54c6411d-1744-0410-a9eb-b99409f43359>
2008-03-27 00:02:29 +0000

    Fixed ApplicationHelper#source_code_version so that it works on Windows where unknown commands return nil rather than a string. Also added logic for displaying Mercurial version.
    git-svn-id: https://calagator.googlecode.com/svn/trunk@451 54c6411d-1744-0410-a9eb-b99409f43359

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,19 +16,21 @@ module ApplicationHelper
     format = format.gsub(/(%[dHImU])/,'*\1')
     time.strftime(format).gsub(/\*0*/,'')
   end
-  
+
+  # Returns HTML for a Google map containing the +locatable_items+.
+  #
+  # Adapt the gmaps_on_rails plugin to talk to our locatable items.
+  # Return markup containing a Google map with markers for these items, or
+  # nil if no items have locations.
+  # - "locatable_items" can be one item or an array of them.
+  # - A locatable item is anything that responds_to? :location and
+  #   :title (yes, our Events and Venues both qualify).
+  # - A locatable item with a nil location will be ignored
+  #
+  # The plugin uses Google Maps automatic zooming to set the scale, but
+  # the little overview map obscures such a big chunk of the main map that
+  # it's likely to hide some of our markers, so it's off by default.
   def google_map(locatable_items, options={})
-    # Adapt the gmaps_on_rails plugin to talk to our locatable items.
-    # Return markup containing a Google map with markers for these items, or
-    # nil if no items have locations.
-    # - "locatable_items" can be one item or an array of them.
-    # - A locatable item is anything that responds_to? :location and 
-    #   :title (yes, our Events and Venues both qualify).
-    # - A locatable item with a nil location will be ignored
-    
-    # The plugin uses Google Maps automatic zooming to set the scale, but 
-    # the little overview map obscures such a big chunk of the main map that
-    # it's likely to hide some of our markers, so it's off by default.
     options[:controls] = [:zoom, :scale, :type] # the default, minus :overview
 
     # Make the map and our marker(s)
@@ -36,7 +38,7 @@ module ApplicationHelper
     [locatable_items].flatten.each do |locatable_item|
       location = locatable_item.location
       if location
-        map.markers << GoogleMapMarker.new(:map => map, 
+        map.markers << GoogleMapMarker.new(:map => map,
           :lat => location[0], :lng => location[1],
           :html => h(locatable_item.title))
       end
@@ -48,13 +50,22 @@ module ApplicationHelper
   def source_code_version
     if File.directory?(File.join(RAILS_ROOT, ".svn"))
       $svn_revision ||= \
-        if m = `svn info`.match(/^Revision: (\d+)/s)
-          "SVN Version: #{m[1]}"
+        if s = `svn info`
+          if m = s.match(/^Revision: (\d+)/s)
+            " - SVN revision: #{m[1]}"
+          end
         end
     elsif File.directory?(File.join(RAILS_ROOT, ".git"))
       $git_date ||= \
-        if m = `git log -1`.match(/^Date: (.+?)$/s)
-          "Git Timestamp: #{m[1]}"
+        if s = `git log -1`
+          if m = s.match(/^Date: (.+?)$/s)
+            " - Git timestamp: #{m[1]}"
+          end
+        end
+    elsif File.directory?(File.join(RAILS_ROOT, ".hg"))
+      $git_date ||= \
+        if s = `hg id -nibt`
+          " - Mercurial revision: #{s}"
         end
     end
   end
