16b4cce671b70575c012c2a880c468225aa4c2bf
igal.koshevoy <igal.koshevoy@54c6411d-1744-0410-a9eb-b99409f43359>
2008-03-29 05:49:41 +0000

    Reworked home page's "Recently Added" list so that it only shows upcoming events that aren't already displayed in the other sections of the page. This changes the meaning of this section, which was previously just a way for letting people confirm that their events were imported/created correctly. However, since we have a notification message popup when they import/create, this is less useful and eats up space. I felt that changing the meaning of this would make this valuable real estate much more useful by displaying many more upcoming events.
    git-svn-id: https://calagator.googlecode.com/svn/trunk@458 54c6411d-1744-0410-a9eb-b99409f43359

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,24 @@
 class HomeController < ApplicationController
   def index
     @events_today = Event.find(:all,
-        :conditions => { :start_time => Time.today..(Time.today + 1.days) }, :order => 'start_time ASC', :limit => 6)
+      :conditions => { :start_time => Time.today..(Time.today + 1.days) },
+      :order => 'start_time ASC',
+      :limit => 6)
     @events_tomorrow = Event.find(:all,
-        :conditions => { :start_time => (Time.today + 1.days)..(Time.today + 2.days) }, :order => 'start_time ASC', :limit => 3)
+      :conditions => { :start_time => (Time.today + 1.days)..(Time.today + 2.days) },
+      :order => 'start_time ASC',
+      :limit => 5)
     @events_later = Event.find(:all,
-        :conditions => { :start_time => (Time.today + 2.days)..(Time.today + 7.days) }, :order => 'start_time ASC', :limit => 3)
-
-    @recently_added_events = Event.find(:all, :order => 'created_at DESC', :limit => 5)
+      :conditions => { :start_time => (Time.today + 2.days)..(Time.today + 7.days) },
+      :order => 'start_time ASC',
+      :limit => 5)
+    @recently_added_events = Event.find(:all,
+      :conditions => ['start_time >= ? AND id NOT IN (?)', Time.today, (@events_today+@events_tomorrow+@events_later).map(&:id)],
+      :order => 'start_time ASC',
+      :limit => 8)
   end
 
+  # Used by #export
   Mime::Type.register "application/sqlite3", :sqlite3
 
   # Export the database
