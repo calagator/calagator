445f2a44933178ba67f0d806c9aad6fcd26a51d5
igal.koshevoy <igal.koshevoy@54c6411d-1744-0410-a9eb-b99409f43359>
2008-03-29 07:36:18 +0000

    Reworked 'next two weeks section' on homepage again so that it's at least a few items long, but otherwise will try to be about the same length as the left side column.
    git-svn-id: https://calagator.googlecode.com/svn/trunk@464 54c6411d-1744-0410-a9eb-b99409f43359

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,14 @@ class HomeController < ApplicationController
   def index
     # TODO Refactor this for clarity
     @events = {}
-    @max_events_per_normal_section = 10
+    @max_events_per_current_section = 10
+    @min_events_per_later_section = (@max_events_per_current_section/2).round
     populate = lambda do |section, opts|
       leaf = {
         :count => Event.count(:conditions => opts[:conditions]),
         :results => Event.find(:all,
           :conditions => opts[:conditions],
-          :limit => (opts[:count] || @max_events_per_normal_section),
+          :limit => (opts[:limit] || @max_events_per_current_section),
           :order => (opts[:order] || 'start_time ASC')),
       }
       leaf[:skipped] = leaf[:count] - (leaf[:results].size || 0)
@@ -18,8 +19,10 @@ class HomeController < ApplicationController
       :conditions => {:start_time => Time.today..(Time.today + 1.days)}}]
     populate[:tomorrow, {
       :conditions => {:start_time => (Time.today + 1.days)..(Time.today + 2.days)}}]
+    current_size = @events[:today][:results].size + @events[:today][:results].size
     populate[:later, {
-      :conditions => {:start_time => (Time.today + 2.days)..(Time.today + 14.days)}}]
+      :conditions => {:start_time => (Time.today + 2.days)..(Time.today + 14.days)},
+      :limit => current_size > @min_events_per_later_section ? current_size : @min_events_per_later_section}]
   end
 
   # Used by #export action
