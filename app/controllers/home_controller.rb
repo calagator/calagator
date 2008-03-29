c9d99e7c662780e5f2124c5ebfd1910701b3207b
igal.koshevoy <igal.koshevoy@54c6411d-1744-0410-a9eb-b99409f43359>
2008-03-29 07:24:46 +0000

    Reworked homepage again. Removed the recently added list entirely and replaced it with a longer 'next two weeks' list.
    git-svn-id: https://calagator.googlecode.com/svn/trunk@463 54c6411d-1744-0410-a9eb-b99409f43359

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     # TODO Refactor this for clarity
     @events = {}
-    @max_events_per_normal_section = 6
+    @max_events_per_normal_section = 10
     populate = lambda do |section, opts|
       leaf = {
         :count => Event.count(:conditions => opts[:conditions]),
@@ -19,14 +19,10 @@ class HomeController < ApplicationController
     populate[:tomorrow, {
       :conditions => {:start_time => (Time.today + 1.days)..(Time.today + 2.days)}}]
     populate[:later, {
-      :conditions => {:start_time => (Time.today + 2.days)..(Time.today + 7.days)}}]
-    populate[:recent, {
-      :conditions => ['start_time >= ? AND id NOT IN (?)',
-        Time.today, @events.values.map{|t| t[:results]}.flatten.map(&:id)],
-      :limit => 10}]
+      :conditions => {:start_time => (Time.today + 2.days)..(Time.today + 14.days)}}]
   end
 
-  # Used by #export
+  # Used by #export action
   Mime::Type.register "application/sqlite3", :sqlite3
 
   # Export the database
