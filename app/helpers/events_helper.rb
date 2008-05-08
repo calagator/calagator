47c3b7965132f7b9a6a5e768cb0dd6cbd571e4ed
bill.burcham <bill.burcham@54c6411d-1744-0410-a9eb-b99409f43359>
2008-05-08 23:58:41 +0000

    Added Google Calendar "reminder" button to each Event (show) page. Close ticket #127.
    git-svn-id: https://calagator.googlecode.com/svn/trunk@578 54c6411d-1744-0410-a9eb-b99409f43359

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,6 +2,8 @@ require 'uri'
 
 module EventsHelper
   include TimeRangeHelper # provides normalize_time
+  
+  GOOGLE_TIME_FORMAT = "%Y%m%dT%H%M%SZ"
 
   def today_tomorrow_or_weekday(record)
     if record.start_time.strftime('%Y:%j') == Time.today.strftime('%Y:%j')
@@ -15,5 +17,17 @@ module EventsHelper
 
   def google_maps_url(address)
     return "http://maps.google.com/maps?q=#{CGI::escape(address)}"
+  end
+  
+  def format_google_timespan( event)
+    end_time = event.end_time || event.start_time
+    "#{portland_to_utc(event.start_time).strftime( GOOGLE_TIME_FORMAT)}/#{portland_to_utc(end_time).strftime( GOOGLE_TIME_FORMAT)}"
+  end
+  
+  private
+  
+  def portland_to_utc( p)
+    # TODO: fix this before Pacific Daylight Time (UTC -7) turns to Pacific Standard Time (UTC -8)
+    DateTime.civil( p.year, p.month, p.day, p.hour + 7, p.min, p.sec)
   end
 end
