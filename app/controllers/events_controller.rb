d09fd9403c2557d50efb6e9544edc31d4ed1a46c
jdcohenesq@gmail.com <jdcohenesq@gmail.com@54c6411d-1744-0410-a9eb-b99409f43359>
2008-05-21 05:24:48 +0000

    Completes Issue94.  This is the Events analog of r629.  Please see comments for r629.
    git-svn-id: https://calagator.googlecode.com/svn/trunk@630 54c6411d-1744-0410-a9eb-b99409f43359

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -137,9 +137,17 @@ class EventsController < ApplicationController
   # GET /events/duplicates
   def duplicates
     @type = params[:type] || 'title'
-    @type = ['all','any'].include?(@type) ? @type.to_sym : @type.split(',')
+
+    if @type == 'na'
+      # Use find to get an Array of all non-duplicates in date order,
+      # make it a Hash so it takes the same form as find_duplicates_by(:grouped => true)
+      # so that the duplicate template will display it properly
+      @grouped_events = { [] => Event.find_all_future_events(:start_time) }
+    else
+      @type = ['all','any'].include?(@type) ? @type.to_sym : @type.split(',')
+      @grouped_events = Event.find_duplicates_by(@type, :grouped => true, :where => "a.start_time >= #{ActiveRecord::Base.connection.quote(Time.now - 1.day)}")
+    end
     
-    @grouped_events = Event.find_duplicates_by(@type, :grouped => true, :where => "a.start_time >= #{ActiveRecord::Base.connection.quote(Time.now - 1.day)}")
     @page_title = "Duplicate Event Squasher"
     
     respond_to do |format|
