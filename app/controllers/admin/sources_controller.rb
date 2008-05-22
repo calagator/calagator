ce03b8e741f40f9fed4cede9fb21a155227d6cdf
igal.koshevoy <igal.koshevoy@54c6411d-1744-0410-a9eb-b99409f43359>
2008-05-22 22:54:02 +0000

    Fixed admin/sources_controller, it no longer shows it's associated events, venues, and updates.
    git-svn-id: https://calagator.googlecode.com/svn/trunk@635 54c6411d-1744-0410-a9eb-b99409f43359

--- app/controllers/admin/sources_controller.rb
+++ app/controllers/admin/sources_controller.rb
@@ -1,6 +1,7 @@
 class Admin::SourcesController < ApplicationController
   layout "admin"
   active_scaffold :source do |config|
-    config.list.columns = [:url, :title, :imported_at, :events]
+    config.list.columns = [:url, :title, :imported_at]
+    config.columns.exclude :events, :venues, :updates
   end
 end
