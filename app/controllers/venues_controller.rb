3d906954ae15a9062c6d3b1161e1729372edbb66
spinnerin <spinnerin@54c6411d-1744-0410-a9eb-b99409f43359>
2008-06-22 00:22:34 +0000

    Making order of venues in duplicate squasher case insensitive.
    git-svn-id: https://calagator.googlecode.com/svn/trunk@854 54c6411d-1744-0410-a9eb-b99409f43359

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -115,7 +115,7 @@ class VenuesController < ApplicationController
       # Use find to get an Array of all non-duplicates in title order,
       # make it a Hash so it takes the same form as find_duplicates_by(:grouped => true)
       # so that the duplicate template will display it properly
-      @grouped_venues = { [] => Venue.find(:non_duplicates, :order => :title) }
+      @grouped_venues = { [] => Venue.find(:non_duplicates, :order => 'lower(title)') }
     else
       @type = ['all','any'].include?(@type) ? @type.to_sym : @type.split(',')
       @grouped_venues = Venue.find_duplicates_by(@type, :grouped => true)
