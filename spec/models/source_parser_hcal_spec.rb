c3942e7a8a42a4bfe105975c8acf339612c29c7f
jdcohenesq <jdcohenesq@54c6411d-1744-0410-a9eb-b99409f43359>
2008-07-17 04:46:07 +0000

    Added some some import/duplicate spec examples. Added some samples. Moved some specs out of source_parser_hcal_spec.rb (and into source_parser_spec.rb) because behavior being tested is invoked at a higher level than source_parser_hcal_spec.rb. Other minor rewording and refactoring.
    git-svn-id: https://calagator.googlecode.com/svn/trunk@973 54c6411d-1744-0410-a9eb-b99409f43359

--- spec/models/source_parser_hcal_spec.rb
+++ spec/models/source_parser_hcal_spec.rb
@@ -1,7 +1,9 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe SourceParser::Hcal, "with hCalendar events" do
-  it "should parse hcal" do
+  fixtures :events, :venues
+
+it "should parse hcal" do
     hcal_content = read_sample('hcal_single.xml')
     hcal_source = Source.new(:title => "Calendar event feed", :url => "http://mysample.hcal/")
     SourceParser::Base.should_receive(:read_url).and_return(hcal_content)
@@ -21,7 +23,16 @@ describe SourceParser::Hcal, "with hCalendar events" do
     end
   end
 
-  it "should parse a page with more than one hcal item in it" do
+  it "should strip html from the venue title" do
+    hcal_content = read_sample('hcal_upcoming.xml')
+    hcal_source = Source.new(:title => "Calendar event feed", :url => "http://mysample.hcal/")
+    SourceParser::Base.stub!(:read_url).and_return(hcal_content)
+    events = hcal_source.to_events
+
+    events.first.venue.title.should == 'Jive Software Office'
+  end
+
+  it "should parse a page with multiple events" do
     hcal_content = read_sample('hcal_multiple.xml')
 
     hcal_source = Source.new(:title => "Calendar event feed", :url => "http://mysample.hcal/")
@@ -35,57 +46,4 @@ describe SourceParser::Hcal, "with hCalendar events" do
     second.start_time.should == Time.parse('2008-2-2')
     second.end_time.should == Time.parse('2008-02-03')
   end
-
-  it "should strip html from the venue title" do
-    hcal_content = read_sample('hcal_upcoming.xml')
-    hcal_source = Source.new(:title => "Calendar event feed", :url => "http://mysample.hcal/")
-    SourceParser::Base.stub!(:read_url).and_return(hcal_content)
-    events = hcal_source.to_events
-
-    events.first.venue.title.should == 'Jive Software Office'
-  end
-
-end
-
-describe SourceParser::Hcal, "when importing events" do
-  fixtures :events, :venues
-
-  it "should not create a new event when importing an identical event" do
-    hcal_content = read_sample('hcal_dup_event_dup_venue.xml')
-    hcal_source = Source.new(:title => "Calendar event feed", :url => "http://mysample.hcal/")
-    SourceParser::Base.stub!(:read_url).and_return(hcal_content)
-
-    events = hcal_source.to_events
-
-    events.first.should_not be_a_new_record # it should return an existing event record and not create a new one
-  end
-
-  it "should create two events when importing two non-identical events"
-
-  it "two events and two venues should be created when importing two identical events with two non-identical venues"
-
-  it "should replace a venue identical to a squashed duplicate with the master venue"  do
-    Event.destroy_all
-    Source.destroy_all
-    Venue.destroy_all
-
-    dummy_source = Source.new(:title => "Dummy", :url => "http://IcalEventWithSquashedVenue.com/")
-    dummy_source.save
-    master_venue = Venue.new(:title => "Master")
-    master_venue.save
-    squashed_venue = Venue.new(
-      :title => "Squashed Duplicate Venue",
-      :duplicate_of_id => master_venue.id)
-    squashed_venue.save
-
-    ical_content = read_sample('ical_event_with_squashed_venue.ics')
-    SourceParser::Base.stub!(:read_url).and_return(ical_content)
-    source = Source.new(:title => "Event with squashed venue", :url => "http://IcalEventWithSquashedVenue.com/")
-
-    events = source.to_events(:skip_old => false)
-
-    event = events.first
-    event.venue.title.should == "Master"
-  end
-
 end
--- spec/models/source_parser_spec.rb
+++ spec/models/source_parser_spec.rb
@@ -6,7 +6,8 @@ end
 describe SourceParser, "when reading content" do
   it "should read from a normal URL" do
     uri = mock_model(URI)
-    uri.should_receive(:respond_to?).and_return(true)
+    # please retain space betwen "?" and ")" on following line; it avoids a SciTE issue
+    uri.should_receive(:respond_to? ).and_return(true)
     uri.should_receive(:read).and_return(42)
     URI.should_receive(:parse).and_return(uri)
 
@@ -15,7 +16,8 @@ describe SourceParser, "when reading content" do
 
   it "should read from a wacky URL" do
     uri = mock_model(URI)
-    uri.should_receive(:respond_to?).any_number_of_times.and_return(false)
+    # please retain space betwen "?" and ")" on following line; it avoids a SciTE issue
+    uri.should_receive(:respond_to? ).any_number_of_times.and_return(false)
     URI.should_receive(:parse).any_number_of_times.and_return(uri)
 
     error_type = RUBY_PLATFORM.match(/mswin/) ? Errno::EINVAL : Errno::ENOENT
@@ -49,5 +51,104 @@ describe SourceParser, "when parsing events" do
     SourceParser::Base.should_receive(:content_for).and_return("fake content")
 
     SourceParser.to_abstract_events(:fake => :argument).should == events
+  end
+end
+
+describe SourceParser, "checking duplicates when importing" do
+  fixtures :events, :venues
+
+  describe "two identical events" do
+    before(:all) do
+      @hcal_source = Source.new(:title => "Calendar event feed", :url => "http://mysample.hcal/")
+      @hcal_content = read_sample('hcal_two_identical_events.xml')
+      SourceParser::Base.stub!(:read_url).and_return(@hcal_content)
+      @parsed_events = @hcal_source.to_events
+    end
+
+    it "should parse two events" do
+      @parsed_events.size.should == 2
+    end
+
+    it "should create only one event" do
+      pending "How to use same data source for parsing and creating?"
+      # in following line, @created_events is array -- after duplicate check -- of created events
+      @created_events.size.should == 1
+    end
+  end
+
+  describe "an event identical to a stored event" do
+    before(:all) do
+      hcal_content = read_sample('hcal_event_duplicates_fixture.xml')
+      hcal_source = Source.new(:title => "Calendar event feed", :url => "http://mysample.hcal/")
+      SourceParser::Base.stub!(:read_url).and_return(hcal_content)
+
+      @events = hcal_source.to_events
+    end
+
+    it "should not create a new event" do
+      @events.first.should_not be_a_new_record
+    end
+
+    it "should return the stored event" do
+      @events.first.title.should == "Web 2.0 Conference"
+    end
+  end
+
+  describe "should create two events when importing two non-identical events" do
+    # This behavior is tested under
+    #  describe SourceParser::Hcal, "with hCalendar events" do
+    #  'it "should parse a page with multiple events" '
+  end
+
+  describe "two identical events with different venues" do
+    before(:all) do
+      hcal_content = read_sample("hcal_same_event_twice_with_different_venues.xml")
+      hcal_source = Source.new(:title => "Calendar event feed", :url => "http://mysample.hcal/")
+      SourceParser::Base.stub!(:read_url).and_return(hcal_content)
+      @parsed_events = hcal_source.to_events
+    end
+
+    it "should parse two events" do
+      @parsed_events.size.should == 2
+    end
+
+    it "should create two events" do
+      pending "How to use same data source for parsing and creating?"
+      # in following line, @created_events is array -- after duplicate check -- of created events
+      @created_events.size.should == 2
+    end
+
+     it "should have different venues for the parsed events" do
+      @parsed_events[0].venue.should_not == @parsed_events[1].venue
+    end
+
+     it "should have different venues for the parsed events" do
+      pending "How to use same data source for parsing and creating?"
+      # in following line, @created_events  is array -- after duplicate check -- of created events
+      @created_events[0].venue.should_not == @created_events[1].venue
+    end
+  end
+
+  it "an event whose venue is identical to a squashed duplicate should use the master venue"  do
+    Event.destroy_all
+    Source.destroy_all
+    Venue.destroy_all
+
+    dummy_source = Source.create(:title => "Dummy", :url => "http://IcalEventWithSquashedVenue.com/")
+    master_venue = Venue.create(:title => "Master")
+    squashed_venue = Venue.create(
+      :title => "Squashed Duplicate Venue",
+      :duplicate_of_id => master_venue.id)
+
+    ical_content = read_sample('ical_event_with_squashed_venue.ics')
+    SourceParser::Base.stub!(:read_url).and_return(ical_content)
+    source = Source.new(
+      :title => "Event with squashed venue",
+      :url => "http://IcalEventWithSquashedVenue.com/")
+
+    events = source.to_events(:skip_old => false)
+
+    event = events.first
+    event.venue.title.should == "Master"
   end
 end
