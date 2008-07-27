d76d00567830a37dd62e2b97e9600b494087f798
jdcohenesq <jdcohenesq@54c6411d-1744-0410-a9eb-b99409f43359>
2008-07-27 23:33:34 +0000

    Moved rowspan calculation from view to helper.  Removed a few unnecessary lines of code.  Used date for some calculations.
    git-svn-id: https://calagator.googlecode.com/svn/trunk@1016 54c6411d-1744-0410-a9eb-b99409f43359

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -4,15 +4,37 @@ module EventsHelper
   GOOGLE_TIME_FORMAT = "%Y%m%dT%H%M%SZ"
 
   def today_tomorrow_or_weekday(record)
-    if record.start_time.strftime('%Y:%j') == Time.today.strftime('%Y:%j')
+    if record.start_time.to_date == Time.today.to_date
       'Today'
-    elsif record.start_time.strftime('%Y:%j') == (Time.today+1.day).strftime('%Y:%j')
+    elsif record.start_time.to_date == (Time.today+1.day).to_date
       'Tomorrow'
     elsif record.ongoing?
       "Started #{record.start_time.strftime('%A')}"
     else
       record.start_time.strftime('%A')
     end
+  end
+
+# calculate rowspans for an array of events
+# argument:  array of events
+# returns:  rowspans, an array in which each entry corresponds to an event in events;
+# each entry is number of rows spanned by today_tomorrow_weekday entry, if any, to left of event
+# entry will be > 0 for first event of day, 0 for other events
+  def calculate_rowspans(events)
+    previous_start_time = nil
+    rowspans = Array.new(events.size, 0)
+    first_event_of_day = 0
+
+    events.each_with_index do |event, index|
+      new_day = previous_start_time.nil? || (previous_start_time.to_date != event.start_time.to_date)
+      if new_day
+        first_event_of_day = index
+      end
+      rowspans[first_event_of_day] += 1
+      previous_start_time = event.start_time
+    end
+
+    return rowspans
   end
 
   def google_maps_url(address)
