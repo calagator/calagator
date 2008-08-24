691b632e98ffddb4004dfff7735f2f9ec499b3e8
igal.koshevoy <igal.koshevoy@54c6411d-1744-0410-a9eb-b99409f43359>
2008-08-24 20:30:53 +0000

    Merged revisions 1085 via svnmerge from https://calagator.googlecode.com/svn/branches/stable
    ........
      r1085 | igal.koshevoy | 2008-08-24 13:25:36 -0700 (Sun, 24 Aug 2008) | 1 line
      
      Improved webadmin: Added svn switch option. Readded db:migrate feature. Added status messages describing current user, branch and revision.
    ........
    
    
    git-svn-id: https://calagator.googlecode.com/svn/trunk@1086 54c6411d-1744-0410-a9eb-b99409f43359

--- webadmin/start.rb
+++ webadmin/start.rb
@@ -1,18 +1,48 @@
-RAILS_ROOT = File.dirname(File.dirname(File.expand_path(__FILE__)))
-RESTART_TXT = "#{RAILS_ROOT}/tmp/restart.txt"
+require 'facets/kernel/ergo'
+
+SVN_REPO     = "https://calagator.googlecode.com/svn/"
+SVN_CHECKOUT = ".."
+RAILS_ROOT   = File.dirname(File.dirname(File.expand_path(__FILE__)))
+RESTART_TXT  = "#{RAILS_ROOT}/tmp/restart.txt"
+
+def get_current_branch
+  return `svn info #{SVN_CHECKOUT}`[/URL: ([^\n]+)/, 1].sub(SVN_REPO, '')
+end
+
+def get_current_revision
+  return `svn info #{SVN_CHECKOUT}`[/Revision: (\d+)/, 1]
+end
 
 get '/' do
+  @branch   = get_current_branch
+  @revision = get_current_revision
+
   haml :index
 end
 
 post '/' do
-  action = params[:action] ? params[:action].match(/(\w+)/)[1] : nil
-  revision = params[:revision] ? params[:revision].match(/(\w+)/)[1] : nil
+  @branch   = get_current_branch
+  @revision = get_current_revision
+
+  action   = params[:action].ergo.to_s[/(\w+)/, 1]
+  revision = params[:revision].ergo.to_s[/(\w+)/, 1]
+  branch   = params[:branch].ergo.to_s[/([\/\w]+)/, 1]
 
   @command = nil
   case action
-  when "deploy"
-    @command = %{svn cleanup && svn update -r #{revision} && touch #{RESTART_TXT}}
+  when "deploy_and_migrate_via_update"
+    if revision
+      @command = %{svn cleanup && svn update -r #{revision} && rake RAILS_ENV=production db:migrate && touch #{RESTART_TXT}}
+    else
+      @message = "ERROR: must specify revision to deploy via update"
+    end
+  when "deploy_and_switch_via_switch"
+    if branch
+      # #{revision.ergo{'-r '+self.to_s}} 
+      @command = %{svn cleanup && svn switch #{SVN_REPO}/#{branch} && rake RAILS_ENV=production db:migrate && touch #{RESTART_TXT}}
+    else
+      @message = "ERROR: must specify branch to deploy via switch"
+    end
   when "restart", "start", "stop", "status"
     @command = %{touch #{RESTART_TXT}}
   end
