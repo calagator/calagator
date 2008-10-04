4d9f67e365a9cca9a620f0b084c244032654ea56
igal.koshevoy <igal.koshevoy@54c6411d-1744-0410-a9eb-b99409f43359>
2008-10-04 10:28:56 +0000

    Added application_helper_spec for #cleanse.
    git-svn-id: https://calagator.googlecode.com/svn/trunk@1197 54c6411d-1744-0410-a9eb-b99409f43359

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,21 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe ApplicationHelper do
+  describe "when escaping HTML while preserving entities (cleanse)" do
+    it "should preserve plain text" do
+      cleanse("Allison to Lillia").should == "Allison to Lillia"
+    end
+    
+    it "should escape HTML" do
+      cleanse("<Fiona>").should == "&lt;Fiona&gt;"
+    end
+
+    it "should preserve HTML entities" do
+      cleanse("Allison &amp; Lillia").should == "Allison &amp; Lillia"
+    end
+
+    it "should handle text, HTML and entities together" do
+      cleanse("&quot;<Allison> &amp; Lillia&quot;").should == "&quot;&lt;Allison&gt; &amp; Lillia&quot;"
+    end
+  end
+end
