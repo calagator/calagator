ffb7ba16e85ef53ac79ddfbfedc78c0542bf78c9
reidab <reidab@54c6411d-1744-0410-a9eb-b99409f43359>
2008-10-11 18:49:35 +0000

    - Installed acts_as_versioned plugin - Added versioning to events - Wrote basic spec checking for the presence of versioning on events.
    git-svn-id: https://calagator.googlecode.com/svn/trunk@1199 54c6411d-1744-0410-a9eb-b99409f43359

---
+++ db/migrate/20081011181519_create_versioned_events.rb
@@ -0,0 +1,21 @@
+class CreateVersionedEvents < ActiveRecord::Migration
+  def self.up
+    Event.create_versioned_table do |t|
+      t.string   "title"
+      t.text     "description"
+      t.datetime "start_time"
+      t.string   "url"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.integer  "venue_id"
+      t.integer  "source_id"
+      t.integer  "duplicate_of_id"
+      t.datetime "end_time"
+      t.integer  "version"
+    end
+  end
+
+  def self.down
+    Event.drop_versioned_table
+  end
+end
