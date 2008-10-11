a05e653a99daf9938c65811dc10fb823b8cceffc
reidab <reidab@54c6411d-1744-0410-a9eb-b99409f43359>
2008-10-11 21:40:05 +0000

    Removing jRails plugin before upgrading to newer version.
    git-svn-id: https://calagator.googlecode.com/svn/trunk@1208 54c6411d-1744-0410-a9eb-b99409f43359

--- vendor/plugins/jrails/init.rb
+++
@@ -1,3 +0,0 @@
-ActionView::Helpers::AssetTagHelper::JAVASCRIPT_DEFAULT_SOURCES = ['jquery','jquery-ui','jquery-fx','jrails']
-ActionView::Helpers::AssetTagHelper::reset_javascript_include_default
-require 'jrails'
