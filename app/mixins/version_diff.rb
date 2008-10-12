f28a40ebdbc979291231ea7e27284f92dc7fa16d
reidab <reidab@54c6411d-1744-0410-a9eb-b99409f43359>
2008-10-12 02:52:20 +0000

    - Added doc comment to VersionDiff - Added spinner to display while loading versions of Events and Venues.
    git-svn-id: https://calagator.googlecode.com/svn/trunk@1222 54c6411d-1744-0410-a9eb-b99409f43359

--- app/mixins/version_diff.rb
+++ app/mixins/version_diff.rb
@@ -1,4 +1,14 @@
 module VersionDiff
+  
+  # provides basic diff services between two given versions.
+  # 
+  # Examples:
+  # @event.version_diff(1) # diff between 1 and head
+  # @event.version_diff(1,2) # diff between 1 and 2
+  # @event.version_diff(:head, 4) #diff between head and 4
+  # @event.version_diff(3,:prev) # diff between 3 and 2
+  # @event.version(:prev) # diff between head and prev
+  #
   def version_diff(n1, n2=:head)
     if n1 == :prev && n2.nil?
       n1 = :head
