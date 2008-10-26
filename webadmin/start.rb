e8736d4b2ffddaa3ad5b5def42e1073ef10e5bd6
igal.koshevoy <igal.koshevoy@54c6411d-1744-0410-a9eb-b99409f43359>
2008-10-26 07:13:19 +0000

    Fixed webadmin, added server:clear task and generalized restart/deploy.
    git-svn-id: https://calagator.googlecode.com/svn/trunk@1262 54c6411d-1744-0410-a9eb-b99409f43359

--- webadmin/start.rb
+++ webadmin/start.rb
@@ -29,22 +29,24 @@ post '/' do
   branch   = params[:branch].ergo.to_s[/([\/\w]+)/, 1]
 
   @command = nil
+  common_restart = %{rake RAILS_ENV=production server:clear tmp:cache:clear && touch #{RESTART_TXT}}
+  common_deploy  = %{rake RAILS_ENV=production db:migrate && #{common_restart}}
   case action
   when "deploy_and_migrate_via_update"
     if revision
-      @command = %{svn cleanup && svn update -r #{revision} && rake RAILS_ENV=production tmp:cache:clear db:migrate && touch #{RESTART_TXT}}
+      @command = %{svn cleanup && svn update -r #{revision} && #{common_deploy}}
     else
       @message = "ERROR: must specify revision to deploy via update"
     end
   when "deploy_and_switch_via_switch"
     if branch
-      # #{revision.ergo{'-r '+self.to_s}} 
-      @command = %{svn cleanup && svn switch #{SVN_REPO}/#{branch} && rake RAILS_ENV=production tmp:cache:clear db:migrate && touch #{RESTART_TXT}}
+      # #{revision.ergo{'-r '+self.to_s}}
+      @command = %{svn cleanup && svn switch #{SVN_REPO}/#{branch} && #{common_deploy}}
     else
       @message = "ERROR: must specify branch to deploy via switch"
     end
   when "restart", "start", "stop", "status"
-    @command = %{rake RAILS_ENV=production tmp:cache:clear && touch #{RESTART_TXT}}
+    @command = %{#{common_restart}}
   end
 
   if @command
