9d4696c8cf39d55550f84167dcd18529727c89b9
igal.koshevoy <igal.koshevoy@54c6411d-1744-0410-a9eb-b99409f43359>
2008-11-04 01:13:59 +0000

    Fixed Issue 319: View caches should be stored on a per server basis
    git-svn-id: https://calagator.googlecode.com/svn/trunk@1270 54c6411d-1744-0410-a9eb-b99409f43359

--- app/models/cacher.rb
+++ app/models/cacher.rb
@@ -4,9 +4,9 @@ require 'fileutils'
 #
 # Provides caching related methods.
 class Cacher
-  # Returns a cache key for the day, e.g. "20080730". It's used primarily by #cache calls in views.
-  def self.daily_key_for(name)
-    return "#{name}@#{Time.now.strftime('%Y%m%d')}"
+  # Returns a cache key for the day, e.g. "20080730". It's used primarily by #cache calls in views. The optional +request+ object can be used to provide a cache prefix so that the caching is specific to a particular hostname.
+  def self.daily_key_for(name, request=nil)
+    return "#{request.ergo{headers['HTTP_HOST'].ergo{to_s+'/'}}}#{name}@#{Time.now.strftime('%Y%m%d')}"
   end
 
   # Expires all cached data.
