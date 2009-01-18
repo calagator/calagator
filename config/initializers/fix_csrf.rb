e87076f4eb08dca3645f743c51b93443d4d4534e
Reid Beels <mail@reidbeels.com>
2009-01-18 03:22:03 -0800

    Removed csrf fix. No longer necessary in rails 2.2

--- config/initializers/fix_csrf.rb
+++
@@ -1,4 +0,0 @@
-# TODO Remove this workaround once it's patched by newer versions of Rails
-# http://feeds.feedburner.com/~r/RidingRails/~3/457453697/potential-circumvention-of-csrf-protection-in-rails-2-1
-
-Mime::Type.unverifiable_types.delete(:text)
