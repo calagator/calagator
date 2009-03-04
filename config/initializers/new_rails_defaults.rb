05b798c8b45d88ca3f383858b04bbbcbdaca1129
Igal Koshevoy <igal@pragmaticraft.com>
2009-03-04 00:37:40 -0800

    Updated initializers/new_rails_defaults to emit existing JSON structures.

--- config/initializers/new_rails_defaults.rb
+++ config/initializers/new_rails_defaults.rb
@@ -3,7 +3,8 @@
 
 if defined?(ActiveRecord)
   # Include Active Record class name as root for JSON serialized output.
-  ActiveRecord::Base.include_root_in_json = true
+  #OVERRIDDEN# ActiveRecord::Base.include_root_in_json = true
+  ActiveRecord::Base.include_root_in_json = false
 
   # Store the full class name (including module namespace) in STI type column.
   ActiveRecord::Base.store_full_sti_class = true
