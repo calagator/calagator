381bb4e73e5a69bda69f7d1122037702a67ecc4f
Igal Koshevoy <igal@pragmaticraft.com>
2009-09-27 17:17:03 -0700

    Updated migrations that referenced the old acts_as_versioned database tables.

--- db/migrate/20081011181519_create_versioned_events.rb
+++ db/migrate/20081011181519_create_versioned_events.rb
@@ -1,21 +1,25 @@
 class CreateVersionedEvents < ActiveRecord::Migration
+  # NOTE: This migration's contents have been commented-out because they relied
+  # on the old +acts_as_versioned+ plugin that has since replaced with the
+  # +papertrail+ plugin.
+
   def self.up
-    Event.create_versioned_table do |t|
-      t.string   "title"
-      t.text     "description"
-      t.datetime "start_time"
-      t.string   "url"
-      t.datetime "created_at"
-      t.datetime "updated_at"
-      t.integer  "venue_id"
-      t.integer  "source_id"
-      t.integer  "duplicate_of_id"
-      t.datetime "end_time"
-      t.integer  "version"
-    end
+###     Event.create_versioned_table do |t|
+###       t.string   "title"
+###       t.text     "description"
+###       t.datetime "start_time"
+###       t.string   "url"
+###       t.datetime "created_at"
+###       t.datetime "updated_at"
+###       t.integer  "venue_id"
+###       t.integer  "source_id"
+###       t.integer  "duplicate_of_id"
+###       t.datetime "end_time"
+###       t.integer  "version"
+###     end
   end
 
   def self.down
-    Event.drop_versioned_table
+###     Event.drop_versioned_table
   end
 end
--- db/migrate/20081011193124_create_versioned_venues.rb
+++ db/migrate/20081011193124_create_versioned_venues.rb
@@ -1,28 +1,32 @@
 class CreateVersionedVenues < ActiveRecord::Migration
+  # NOTE: This migration's contents have been commented-out because they relied
+  # on the old +acts_as_versioned+ plugin that we've since replaced with the
+  # +papertrail+ plugin.
+  #
   def self.up
-    Venue.create_versioned_table do |t|
-      t.string   "title"
-      t.text     "description"
-      t.string   "address"
-      t.string   "url"
-      t.datetime "created_at"
-      t.datetime "updated_at"
-      t.string   "street_address"
-      t.string   "locality"
-      t.string   "region"
-      t.string   "postal_code"
-      t.string   "country"
-      t.decimal  "latitude"
-      t.decimal  "longitude"
-      t.string   "email"
-      t.string   "telephone"
-      t.integer  "source_id"
-      t.integer  "duplicate_of_id"
-      t.integer  "version"
-    end
+###     Venue.create_versioned_table do |t|
+###       t.string   "title"
+###       t.text     "description"
+###       t.string   "address"
+###       t.string   "url"
+###       t.datetime "created_at"
+###       t.datetime "updated_at"
+###       t.string   "street_address"
+###       t.string   "locality"
+###       t.string   "region"
+###       t.string   "postal_code"
+###       t.string   "country"
+###       t.decimal  "latitude"
+###       t.decimal  "longitude"
+###       t.string   "email"
+###       t.string   "telephone"
+###       t.integer  "source_id"
+###       t.integer  "duplicate_of_id"
+###       t.integer  "version"
+###     end
   end
 
   def self.down
-    Venue.drop_versioned_table
+###     Venue.drop_versioned_table
   end
 end
