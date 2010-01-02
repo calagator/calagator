0871acee03f82c275c7b2de86a783d1a8af9de83
Igal Koshevoy <igal@pragmaticraft.com>
2010-01-01 21:30:05 -0800

    Updated configurations for use with Super Exception Notifier.

--- config/initializers/exception_notification_setup.rb
+++ config/initializers/exception_notification_setup.rb
@@ -1,6 +1,9 @@
 require 'etc'
 require 'socket'
 
-ExceptionNotifier.exception_recipients = [SECRETS.administrator_email]
-ExceptionNotifier.email_prefix = "[ERROR #{SETTINGS.name}] "
-ExceptionNotifier.sender_address = "#{Etc.getlogin}@#{Socket.gethostname}"
+ExceptionNotifier.configure_exception_notifier do |c|
+  c[:exception_recipients] = [SECRETS.administrator_email]
+  c[:sender_address] = "#{Etc.getlogin}@#{Socket.gethostname}"
+  c[:subject_prepend] = "[ERROR #{SETTINGS.name}] "
+  c[:skip_local_notification]  = false
+end
