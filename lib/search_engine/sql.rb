02d63718643ad2a6d822d8bccb52cf9666f684a1
Igal Koshevoy <igal@pragmaticraft.com>
2011-02-06 18:51:18 -0800

    Added SearchEngine::Sql classes for pure SQL search.

---
+++ lib/search_engine/sql.rb
@@ -0,0 +1,5 @@
+require 'lib/search_engine/base'
+
+class SearchEngine::Sql < SearchEngine::Base
+  score false
+end
---
+++ lib/search_engine/sql/venue.rb
@@ -0,0 +1,5 @@
+class SearchEngine::Sql::Venue
+  def self.add_searching_to(model)
+    false
+  end
+end
