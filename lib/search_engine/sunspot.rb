12fb0ca93a1567badaf1646e58e70eaa881fdd13
Igal Koshevoy <igal@pragmaticraft.com>
2011-03-01 13:38:46 -0800

    Fixed SearchEngine::Sunspot so `rake spec` works w/out Sunspot installed.
    The SearchEngine::Sunspot driver class was originally written with the 
    assumption that it would only ever be loaded if Sunspot was selected as the
    search engine. 
    
    Unfortunately, the Rails test environment loads all classes that in the 
    autoload paths it finds, while the development and test environments only
    load those they need. This was a problem because if a person doesn't have
    Sunspot installed and configured, this class would be loaded and cause the
    execution of the specs to fail.
    
    The fix is to only monkey patch Sunspot if it's present so this class can be
    loaded safely even if Sunspot isn't being used.

--- lib/search_engine/sunspot.rb
+++ lib/search_engine/sunspot.rb
@@ -117,17 +117,19 @@ class SearchEngine::Sunspot < SearchEngine::Base
   end
 end
 
-# Monkeypatch Sunspot to connect to Solr server. Sigh. For details, see:
-# http://groups.google.com/group/ruby-sunspot/browse_thread/thread/34772773b4b5682d
-module Sunspot::Rails
-  def slave_config(sunspot_rails_configuration)
-    config = Sunspot::Configuration.build
-    config.solr.url = URI::HTTP.build(
-        :host => sunspot_rails_configuration.hostname,
-        :port => sunspot_rails_configuration.port,
-        :path => sunspot_rails_configuration.path
-      ).to_s
-    config
+if defined?(Sunspot)
+  # Monkeypatch Sunspot to connect to Solr server. Sigh. For details, see:
+  # http://groups.google.com/group/ruby-sunspot/browse_thread/thread/34772773b4b5682d
+  module Sunspot::Rails
+    def slave_config(sunspot_rails_configuration)
+      config = Sunspot::Configuration.build
+      config.solr.url = URI::HTTP.build(
+          :host => sunspot_rails_configuration.hostname,
+          :port => sunspot_rails_configuration.port,
+          :path => sunspot_rails_configuration.path
+        ).to_s
+      config
+    end
   end
+  Sunspot.session = Sunspot::Rails.build_session
 end
-Sunspot.session = Sunspot::Rails.build_session
