f274da50bdb76f872c0fa68980a8bab7d109b7fa
Igal Koshevoy <igal@pragmaticraft.com>
2011-03-21 17:20:25 -0700

    Increase changes#show to display 50 changes per page, not 20.

--- app/controllers/changes_controller.rb
+++ app/controllers/changes_controller.rb
@@ -1,6 +1,6 @@
 class ChangesController < ApplicationController
   def show
-    @versions = Defer { ::Version.paginate(:page => params[:page], :order => 'created_at desc', :per_page => 20) }
+    @versions = Defer { ::Version.paginate(:page => params[:page], :order => 'created_at desc', :per_page => 50) }
     respond_to do |format|
       format.html # changes.html.erb
       format.atom # changes.atom.builder
