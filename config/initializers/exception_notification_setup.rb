41c1083202a3daf6bfbce5a76dd6479d78eedcc1
Igal Koshevoy <igal@pragmaticraft.com>
2011-04-29 12:14:36 -0700

    Send exception notification from same address its sent to, so it can be replied to.

--- config/initializers/exception_notification_setup.rb
+++ config/initializers/exception_notification_setup.rb
@@ -3,7 +3,7 @@ require 'socket'
 
 ExceptionNotifier.configure_exception_notifier do |c|
   c[:exception_recipients] = [SECRETS.administrator_email]
-  c[:sender_address] = "#{Etc.getlogin}@#{Socket.gethostname}"
+  c[:sender_address] = "#{SECRETS.administrator_email}"
   c[:subject_prepend] = "[ERROR #{SETTINGS.name}] "
   c[:skip_local_notification]  = false
 end
