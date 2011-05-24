767b97e4c4be246bd092cf2b036ba6fd64276b56
Igal Koshevoy <igal@pragmaticraft.com>
2011-05-23 20:07:06 -0700

    Add bundler boostrap.

--- config/boot.rb
+++ config/boot.rb
@@ -110,5 +110,20 @@ module Rails
   end
 end
 
+# Added for bundler: http://gembundler.com/rails23.html
+class Rails::Boot
+  def run
+    load_initializer
+
+    Rails::Initializer.class_eval do
+      def load_gems
+        @bundler_loaded ||= Bundler.require :default, Rails.env
+      end
+    end
+
+    Rails::Initializer.run(:set_load_path)
+  end
+end
+
 # All that for this:
 Rails.boot!
