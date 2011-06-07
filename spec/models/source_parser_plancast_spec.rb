9e4ffa326b756b3ddaca6d465e07226a94f1ab8d
Igal Koshevoy <igal@pragmaticraft.com>
2011-06-07 05:04:49 -0700

    Fix source_parser_plancast_spec to not fail because sample event is now in past.

--- spec/models/source_parser_plancast_spec.rb
+++ spec/models/source_parser_plancast_spec.rb
@@ -7,7 +7,8 @@ describe SourceParser::Plancast do
     content = read_sample('plancast.ics')
     SourceParser::Base.should_receive(:read_url).and_return(content)
     events = SourceParser::Plancast.to_abstract_events(:content => content,
-                                                       :url => 'http://plancast.com/p/5px4/pdx11-civic-hackathon')
+                                                       :url => 'http://plancast.com/p/5px4/pdx11-civic-hackathon',
+                                                       :skip_old => false)
 
     events.size.should == 1
     events.first.tags.should == ["plancast:plan=5px4"]
