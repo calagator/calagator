3af5025995e42ab38cdbbe98b1cfa6cdda0dfe79
Reid Beels <mail@reidbeels.com>
2011-07-11 15:50:27 -0700

    Update Facebook parser spec to work with improved Facebook source parser error reporting

--- spec/models/source_parser_facebook_spec.rb
+++ spec/models/source_parser_facebook_spec.rb
@@ -4,7 +4,9 @@ describe SourceParser::Facebook do
 
   before(:each) do
     content = read_sample('facebook.json')
-    HTTParty.should_receive(:get).and_return(Crack::JSON.parse(content))
+    parsed_content = Crack::JSON.parse(content)
+    parsed_content.should_receive(:parsed_response).and_return(parsed_content)
+    HTTParty.should_receive(:get).and_return(parsed_content)
     @events = SourceParser::Facebook.to_abstract_events(:content => content,
                                                        :url => 'http://facebook.com/event.php?eid=247619485255249',
                                                        :skip_old => false)
