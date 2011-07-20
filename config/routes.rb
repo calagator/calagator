915f53f825bf63d5a2f396565574b38425d503aa
Igal Koshevoy <igal@pragmaticraft.com>
2011-07-19 22:12:23 -0700

    Fix routes indentation.

--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,35 @@
 Calagator::Application.routes.draw do
   match 'omfg' => 'site#omfg'
   match 'hello' => 'site#hello'
+
   match 'about' => 'site#about'
+
   match 'opensearch.:format' => 'site#opensearch'
 
   resources :events do
     collection do
-  post :squash_multiple_duplicates
-  get :search
-  get :duplicates
-  end
-    member do
-  get :clone
-  end
+      post :squash_multiple_duplicates
+      get :search
+      get :duplicates
+    end
 
+    member do
+      get :clone
+    end
   end
 
   resources :sources do
     collection do
-  put :import
-  end
-  
-
+      put :import
+    end
   end
 
   resources :venues do
     collection do
-  post :squash_multiple_duplicates
-  get :map
-  get :duplicates
-  end
-
-
+      post :squash_multiple_duplicates
+      get :map
+      get :duplicates
+    end
   end
 
   resources :versions, :only => [:edit]
@@ -41,8 +39,10 @@ Calagator::Application.routes.draw do
 
   match 'export' => 'site#export'
   match 'export.:format' => 'site#export'
+
   match 'css/:name' => 'site#style'
   match 'css/:name.:format' => 'site#style'
+
   match '/' => 'site#index', :as => :root
 
   themes_for_rails
