9094425291de564d956e61b4b8156e526f162da0
Peter W <the.peter.w@gmail.com>
2011-11-10 14:21:31 -0800

    initial commit of organization model+ empty test

---
+++ app/models/organization.rb
@@ -0,0 +1,2 @@
+class Organization < ActiveRecord::Base
+end
---
+++ spec/models/organization_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Organization do
+  pending "add some examples to (or delete) #{__FILE__}"
+  
+  # describe "in general" do
+  # 	it "should be valid" do 
+  # 		organization = Organization.new(:name => "Org Name", :url => "http://url")
+end
