0613a608e97cd704ae9248d54909accdcf7d22ee
Pam Selle <pamela.selle@gmail.com>
2011-11-12 13:54:12 -0800

    Added search for venues in sunspot and sql cases. Removed order by title in venues controller because that's the default anyway.

--- lib/search_engine/sql.rb
+++ lib/search_engine/sql.rb
@@ -5,8 +5,48 @@ class SearchEngine::Sql < SearchEngine::Base
 
   def self.add_searching_to(model)
     case model.new
-    when Venue, Source
+    when Source
       # Do nothing
+    when Venue
+      model.class_eval do
+      # Return an Array of non-duplicate Venue instances matching the search +query+..
+        #
+        # Options:
+        # * :order => How to order the entries? Defaults to :name. Permitted values:
+        #   * :name => Sort by event title
+        #   * :title => same as :name
+        # * :limit => Maximum number of entries to return. Defaults to +solr_search_matches+.
+        # * :wifi => Require wifi
+        # * :include_closed => Include closed venues? Defaults to false.
+        def self.search(query, opts={})
+          wifi = opts[:wifi]
+          include_closed = opts[:include_closed] == true
+          limit = opts[:limit] || 50
+
+          scoped_venues = Venue.non_duplicates
+          # Pick a subset of venues (we want in_business by default)
+          unless include_closed
+            scoped_venues = scoped_venues.in_business
+          end
+
+          scoped_venues = scoped_venues.with_public_wifi if wifi
+
+          order = \
+            case opts[:order].try(:to_sym)
+            when nil, :name, :title
+              'LOWER(venues.title) ASC'
+            else
+              raise ArgumentError, "Unknown order: #{order}"
+            end
+
+          keywords = query.split(" ")
+          tag_conditions = Array.new(keywords.size, "LOWER(tags.name) = ?").join(" OR ")
+
+          conditions = ["title LIKE ? OR description LIKE ? OR (#{tag_conditions})", *(["%#{query}%", "%#{query}%"] + keywords) ]
+          return scoped_venues.joins('LEFT OUTER JOIN taggings on taggings.taggable_id = venues.id AND taggings.taggable_type = "Venue"',
+                                     'LEFT OUTER JOIN tags ON tags.id = taggings.tag_id').where(conditions).order(order).group(Venue.columns.map(&:name).map{|attribute| "venues.#{attribute}"}.join(', ')).limit(limit)
+        end
+      end
     when Event
       model.class_eval do
         # Return an Array of non-duplicate Event instances matching the search +query+..
@@ -48,7 +88,8 @@ class SearchEngine::Sql < SearchEngine::Base
             conditions_arguments = [Date.yesterday.to_time] + conditions_arguments
           end
           conditions = [conditions_text, *conditions_arguments]
-          return Event.joins(:taggings, 'LEFT OUTER JOIN tags ON tags.id = taggings.tag_id').includes(:venue).where(conditions).order(order).group(Event.columns.map(&:name).map{|attribute| "events.#{attribute}"}.join(', ')).limit(limit)
+          return Event.joins('LEFT OUTER JOIN taggings on taggings.taggable_id = events.id AND taggings.taggable_type = "Event"',
+                             'LEFT OUTER JOIN tags ON tags.id = taggings.tag_id').includes(:venue).where(conditions).order(order).group(Event.columns.map(&:name).map{|attribute| "events.#{attribute}"}.join(', ')).limit(limit)
         end
       end
     else
