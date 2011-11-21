c53e128d245a22431b398581da9f333d1d4fddf6
Peter W <the.peter.w@gmail.com>
2011-11-21 15:13:40 -0800

    bringing in code from other controllers (broken)

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,12 +1,29 @@
 class OrganizationsController < ApplicationController
   def index
-  	@organizations = Organization.all
+    @organizations = Organization.all
   end
 
+  # GET /organizations/1
   def show
+    begin
+      @organization = Organization.find(params[:id])
+    rescue ActiveRecord::RecordNotFound => e
+      return redirect_to organizations_path, :flash => {:failure => e.to_s}
+    end
+
+    @page_title = @organization.name
   end
 
+  # GET /organizations/new
+  # GET /organizations/new.xml
   def new
+    @organization = Organization.new(params[:organization])
+    @page_title = "Add an Organization"
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @organization }
+    end
   end
 
   def edit
