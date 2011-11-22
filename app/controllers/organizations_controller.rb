79a083efd32c77d1bb5ab92606bf685c71dde962
Peter W <the.peter.w@gmail.com>
2011-11-21 20:43:08 -0800

    First pass at Organization controller+view

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -4,14 +4,21 @@ class OrganizationsController < ApplicationController
   end
 
   # GET /organizations/1
+  # GET /organizations/1.xml
   def show
     begin
-      @organization = Organization.find(params[:id])
+      @organization = Organization.find(params[:id], :include => [:events])
     rescue ActiveRecord::RecordNotFound => e
-      return redirect_to organizations_path, :flash => {:failure => e.to_s}
+      flash[:failure] = e.to_s
+      return redirect_to(new_organization_path)
     end
 
     @page_title = @organization.name
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @organization }
+    end
   end
 
   # GET /organizations/new
@@ -26,7 +33,47 @@ class OrganizationsController < ApplicationController
     end
   end
 
+  # GET /organizations/1/edit
   def edit
+    @organization = Organization.find(params[:id])
   end
 
+
+  # POST /organizations
+  # POST /organizations.xml
+  def create
+    @organization = Organization.new(params[:organization])
+
+    if evil_robot = !params[:trap_field].blank?
+      flash[:failure] = "<h3>Evil Robot</h3> We didn't create this organization because we think you're an evil robot. If you're really not an evil robot, look at the form instructions more carefully. If this doesn't work please file a bug report and let us know."
+    end
+
+    respond_to do |format|
+      if !evil_robot && @organization.save
+        flash[:success] = 'Organization was successfully created.'
+        format.html { redirect_to( organization_path(@organization) ) }
+        format.xml  { render :xml => @organization, :status => :created, :location => @organization }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @organization.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /organizations/1
+  # PUT /organizations/1.xml
+  def update
+    @organization = Organization.find(params[:id])
+
+    respond_to do |format|
+      if @organization.update_attributes(params[:organization])
+        flash[:notice] = 'Organization was successfully updated.'
+        format.html { redirect_to( organization_path(@organization) ) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @organization.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
 end
