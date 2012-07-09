859e2e6ee2c8a3606c90b34546aa5adce6291fe3
Igal Koshevoy <igal@pragmaticraft.com>
2012-07-09 03:03:11 -0700

    Use Capistrano for calagator.org.

--- config/deploy/calagator.rb
+++ config/deploy/calagator.rb
@@ -3,8 +3,7 @@ set :theme, "calagator"
 set :scm, "git"
 set :branch, "master" unless variables[:branch]
 set :repository,  "git://github.com/calagator/calagator.git"
-# set :deploy_to, "/var/www/calagator"
-set :deploy_to, "/tmp/calagator"
+set :deploy_to, "/var/www/calagator"
 set :host, "calagator.org"
 set :user, "calagator"
 
