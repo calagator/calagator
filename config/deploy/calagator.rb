3f79c1d2bf649684365c05a8ad3ad74284c34c3d
Igal Koshevoy <igal@pragmaticraft.com>
2012-08-11 15:53:25 -0700

    Capistrano: Use a local copy deploy strategy.

--- config/deploy/calagator.rb
+++ config/deploy/calagator.rb
@@ -1,13 +1,21 @@
 set :theme, "calagator"
 
+# set :scm, "git"
+# set :branch, "master" unless variables[:branch]
+# set :repository,  "git://github.com/calagator/calagator.git"
+# set :deploy_via, :remote_cache
+
 set :scm, "git"
-set :branch, "master" unless variables[:branch]
-set :repository,  "git://github.com/calagator/calagator.git"
+set :repository,  "."
+set :deploy_via, :copy
+set :copy_cache, true
+set :copy_exclude, ['.git', 'log', 'tmp', '*.sql', '*.diff', 'coverage.info', 'coverage.info', 'coverage', 'public/images/members', 'public/system', 'tags', 'db/remote.sql', 'db/*.sqlite3', '*.swp', '.*.swp']
+default_run_options[:pty] = true
+
 set :deploy_to, "/var/www/calagator"
 set :host, "calagator.org"
 set :user, "calagator"
 
-set :deploy_via, :remote_cache
 role :app, host
 role :web, host
 role :db,  host, :primary => true
