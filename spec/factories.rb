b078c1e1343a9868b8119be64433fce54d9f3ad3
Igal Koshevoy <igal@pragmaticraft.com>
2012-08-31 17:56:41 -0700

    Fix :venue factory use sensible latitude/longitude. Add more fields.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,15 +1,17 @@
 Factory.define :venue do |f|
   f.sequence(:title) { |n| "Venue #{n}" }
   f.sequence(:description) { |n| "Description of Venue #{n}." }
+  f.sequence(:address) { |n| "Address #{n}" }
   f.sequence(:street_address) { |n| "Street #{n}" }
   f.sequence(:locality) { |n| "City #{n}" }
-  f.sequence(:region) { |n| "region #{n}" }
+  f.sequence(:region) { |n| "Region #{n}" }
   f.sequence(:postal_code) { |n| "#{n}-#{n}-#{n}" }
   f.sequence(:country) { |n| "Country #{n}" }
-  f.sequence(:latitude) { |n| n }
-  f.sequence(:longitude) { |n| n }
+  f.sequence(:latitude) { |n| "45.#{n}".to_f }
+  f.sequence(:longitude) { |n| "122.#{n}".to_f }
   f.sequence(:email) { |n| "info@venue#{n}.com" }
   f.sequence(:telephone) { |n| "(#{n}#{n}#{n}) #{n}#{n}#{n}-#{n}#{n}#{n}#{n}" }
+  f.sequence(:url) { |n| "http://#{n}.com" }
   f.closed false
   f.wifi true
   f.access_notes "Access permitted."
