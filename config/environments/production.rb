3dceac36bcca02b84fb3ab1eb443061e23ba55d4
Daniel Hedlund <daniel@digitree.org>
2013-06-24 13:32:40 -0700

    Take advantage of asset pipeline for some core assets [THEME] [DEPLOY]
    Removed some core assets (jquery, jquery-ui, formtastic) that are
    automatically provided by gems; before the asset pipeline, assets had to be
    copied across to the project directory by hand or via *:install rake tasks.
    
    Removed some assets that didn't appear to be used by anything such as
    reset-fonts-grids.css, jquery.dimensions.min.js.
    
    [THEME] The 'reset' stylesheet was moved to core, it is unlikely you would
    need or want to update this manually.  'common' and 'formtastic' core
    stylesheets have been merged into the 'application' stylesheet.
    
      Change the following lines in themes/*/views/layouts/application.html.erb:
        <%= theme_stylesheet_link_tag 'reset', :media => :all %>
        <%= stylesheet_link_tag 'common', 'formtastic', :media => :all %>
      to look like:
        <%= stylesheet_link_tag 'application', :media => :all %>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Calagator::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( *.js *.css )
+  config.assets.precompile += %w( forms.css forms.js mustache.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
--- config/initializers/themes.rb
+++ config/initializers/themes.rb
@@ -1,4 +1,12 @@
 ThemesForRails.config do |config|
   # your static assets on Rails.root/themes/pink/assets/{stylesheets,javascripts,images}
-  config.assets_dir = ":root/themes/:name" 
+  config.assets_dir = ":root/themes/:name"
+
+  # UPSTREAM BUG: the themes:create_cache rake task in the ThemesForRails
+  # gem has a bug when trying to figure out where to copy assets; it
+  # assumes the themes directory is a relative path w/o a ':root'
+  # placeholder, even though the gem's default is ':root/themes'. This
+  # line can be removed once it has been determined that the rake task
+  # has been fixed upstream.
+  config.themes_dir = 'themes'
 end
