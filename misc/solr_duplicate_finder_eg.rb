7ff7faa4b68a4c16f2a2d8219fa180e0c513b2bd
Micah Geisel <micah@botandrose.com>
2014-07-03 02:04:19 -0700

    none of this has been touched in 6 years. remove.

--- misc/solr_duplicate_finder_eg.rb
+++
@@ -1,5 +0,0 @@
-# TODO rework text_for_solr to remove non-word characters
-event = Event.find(:first)
-query = SolrQuery.new{Fuzzy(event.text_for_solr)}.to_s.gsub(/[^\w\s]/, ' ')
-rs = Event.find_by_solr(query, :scores => true, :order => "score asc")
-
