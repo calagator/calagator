2e33b249a657918fd3c5e689b3da910e58a1c236
Micah Geisel <micah@botandrose.com>
2014-07-12 18:27:32 -0700

    refactor Event::Search::Sunspot.

--- app/models/event/search/sunspot.rb
+++ app/models/event/search/sunspot.rb
@@ -12,7 +12,50 @@ class Event < ActiveRecord::Base
       #   * :venue => Sort by venue title
       # * :limit => Maximum number of entries to return. Defaults to +solr_search_matches+.
       # * :skip_old => Return old entries? Defaults to false.
-      def self.search(query, opts={})
+      def self.search(*args)
+        new(*args).search
+      end
+
+      def initialize(*args)
+        super
+        configure unless configured?
+      end
+
+      def search
+        Event.solr_search do
+          keywords query, minimum_match: 1
+          order_by *order
+          order_by :start_time, :desc
+          with :duplicate, false
+          with(:start_time).greater_than(Date.yesterday.to_time) if skip_old
+          data_accessor_for(Event).include = [:venue]
+        end.results.take(limit)
+      end
+
+      private
+
+      def order
+        case opts[:order].try(:to_sym)
+        when :date
+          [:start_time, :desc]
+        when :venue, :location
+          [:venue_title, :asc]
+        when :name, :title
+          [:title, :asc]
+        else
+          [:score, :desc]
+        end
+      end
+
+      def skip_old
+        opts[:skip_old] == true
+      end
+
+      def limit
+        opts[:limit] || 50
+      end
+
+      def configure
         Event.searchable do
           text :title, :default_boost => 3
           string :title
@@ -31,42 +74,10 @@ class Event < ActiveRecord::Base
 
           boolean(:duplicate) { |event| event.duplicate? }
         end
+      end
 
-        skip_old = opts[:skip_old] == true
-        limit = opts[:limit] || 50
-
-        # This method fetches events using two separate searches. Current
-        # events are fetched and sorted by default using relevance scores, so
-        # that the most meaningful events are at the top. Past events are
-        # fetched and sorted by default using date, so that the most recent
-        # events involving the search term are at the top.
-        ordering = \
-          case opts[:order].try(:to_sym)
-          when :date
-            [:start_time, :desc]
-          when :venue, :location
-            [:venue_title, :asc]
-          when :name, :title
-            [:title, :asc]
-          when :score
-            [:score, :desc]
-          else
-            nil
-          end
-
-        searcher = Event.solr_search do
-          keywords query, minimum_match: 1
-          # FIXME figure out how to implement selective substring matching
-          # keywords "*#{query}*", minimum_match: 1, fields: :url
-          ordering ?
-            order_by(*ordering) :
-            order_by(:score, :desc)
-            order_by(:start_time, :desc)
-          with(:duplicate, false)
-          with(:start_time).greater_than(Date.yesterday.to_time) if opts[:skip_old]
-          data_accessor_for(Event).include = [:venue]
-        end
-        searcher.results.take(limit)
+      def configured?
+        Event.respond_to?(:solr_search)
       end
     end
   end
