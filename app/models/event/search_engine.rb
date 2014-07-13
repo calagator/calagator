2e82a8e3c7f023f693b6b38ff4de5e1be3acff8b
Micah Geisel <micah@botandrose.com>
2014-07-12 19:46:31 -0700

    move all knowledge of search engine choice into Event::SearchEngine.

--- app/models/event/search_engine.rb
+++ app/models/event/search_engine.rb
@@ -1,5 +1,15 @@
 class Event < ActiveRecord::Base
   class SearchEngine
     cattr_accessor(:kind) { :sql }
+
+    def self.search(*args)
+      search_engine.search(*args)
+    end
+
+    private_class_method
+
+    def self.search_engine
+      kind == :sunspot ? Sunspot : Sql
+    end
   end
 end
