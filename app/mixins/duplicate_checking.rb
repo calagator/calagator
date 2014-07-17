eb6a1ae35248bbeb42f2c0d287db0e9cd3669055
Micah Geisel <micah@botandrose.com>
2014-07-16 17:39:38 -0700

    extract DuplicateChecking::DuplicateSquasher.

--- app/mixins/duplicate_checking.rb
+++ app/mixins/duplicate_checking.rb
@@ -114,59 +114,7 @@ module DuplicateChecking
     # :duplicates => ActiveRecord instance(s) to mark as duplicates
     # :master => ActiveRecord instance to use as master
     def squash(opts)
-      master     = opts[:master]
-      duplicates = Array(opts[:duplicates])
-
-      raise(ArgumentError, ":master not specified")     if master.blank?
-      raise(ArgumentError, ":duplicates not specified") if duplicates.blank?
-
-      squashed = []
-
-      duplicates.each do |duplicate|
-        next if !master.new_record? && !duplicate.new_record? && duplicate.id == master.id
-
-        # Transfer any venues that use this now duplicate venue as a master
-        unless duplicate.duplicates.blank?
-          Rails.logger.debug("#{self.name}#squash: recursively squashing children of #{self.name}@#{duplicate.id}")
-          squash(:master => master, :duplicates => duplicate.duplicates)
-        end
-
-        # Transfer any has_many associations of this model to the master
-        reflect_on_all_associations(:has_many).each do |association|
-          next if association.name == :duplicates
-          if duplicate_squashing_ignores_associations.include?(association.name.to_sym)
-            Rails.logger.debug(%{#{name}#squash: skipping assocation '#{association.name}'})
-            next
-          end
-
-          # Handle tags - can't simply reassign, need to be unique, and they may have some of the same tags
-          if association.name == :tag_taggings
-            squash_tags(master, duplicate)
-            next
-          end
-
-          foreign_objects = duplicate.send(association.name)
-          foreign_objects.each do |object|
-            object.update_attribute(association.foreign_key, master.id) unless object.new_record?
-            Rails.logger.debug(%{#{name}#squash: transferring foreign object "#{object.class.name}##{object.id}" from duplicate "#{name}##{duplicate.id}" to master "#{name}##{master.id}" via association "#{association.name}" using attribute "#{association.foreign_key}"})
-          end
-        end
-
-        # TODO: Add support for habtm and other associations
-
-        # Mark this as a duplicate
-        duplicate.duplicate_of = master
-        duplicate.update_attribute(:duplicate_of, master) unless duplicate.new_record?
-        squashed << duplicate
-        Rails.logger.debug("#{name}#squash: marking #{name}@#{duplicate.id} as duplicate of #{name}@#{master.id}")
-      end
-      squashed
-    end
-
-    # custom behavior for tags, concatentate the two objects tag strings together
-    def squash_tags(master, duplicate)
-      master.tag_list = master.tag_list + duplicate.tag_list
-      master.save_tags
+      DuplicateSquasher.new(self, opts).squash
     end
   end
 end
