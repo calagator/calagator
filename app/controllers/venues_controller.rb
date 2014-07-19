c1bbfa32c01996da9d404abcb4bccb61e2f372b9
Micah Geisel <micah@botandrose.com>
2014-07-19 00:17:19 -0700

    extract Venue::Search from VenuesController#index.

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -4,39 +4,12 @@ class VenuesController < ApplicationController
   # GET /venues
   # GET /venues.xml
   def index
-    scoped_venues = Venue.non_duplicates
-
-    # Pick a subset of venues (we want in_business by default)
-    if params[:include_closed]
-      scoped_venues = scoped_venues
-    elsif params[:closed]
-      scoped_venues = scoped_venues.out_of_business
-    else
-      scoped_venues = scoped_venues.in_business
-    end
-
-    # Support old ajax autocomplete parameter name
-    params[:term] = params[:val] if params[:val]
-
-    @tag = nil
-    if params[:tag].present? # searching by tag
-      @tag = params[:tag]
-      @venues = scoped_venues.tagged_with(@tag)
-    elsif params.has_key?(:query) || params.has_key?(:term) || params[:all] == '1' # searching by query
-      scoped_venues = scoped_venues.with_public_wifi if params[:wifi]
-
-      if params[:term].present? # for the ajax autocomplete widget
-        conditions = ["title LIKE ?", "%#{params[:term]}%"]
-        @venues = scoped_venues.where(conditions).order('LOWER(title)')
-      elsif params[:query].present?
-        @venues = Venue.search(params[:query], :include_closed => params[:include_closed], :wifi => params[:wifi])
-      else
-        @venues = scoped_venues.all
-      end
-    else # default view
-      @most_active_venues = scoped_venues.limit(10).order('events_count DESC')
-      @newest_venues = scoped_venues.limit(10).order('created_at DESC')
-    end
+    search = Venue::Search.new(params)
+    @venues = search.venues
+    @tag = search.tag
+    @most_active_venues = search.most_active_venues
+    @newest_venues = search.newest_venues
+    scoped_venues = search.scoped_venues
 
     @page_title = "Venues"
 
