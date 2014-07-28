907dcf68e580516210fc67c946a7de771a578358
Mark Lehman <markopolo@gmail.com>
2014-07-27 22:20:15 -0700

    add defunct link handling to machine tag generator, routes, and controller #145

--- lib/tag_model_extensions.rb
+++ lib/tag_model_extensions.rb
@@ -83,6 +83,11 @@ module TagModelExtensions
       if machine_tag = MACHINE_TAG_URLS[namespace]
         if url_template = machine_tag[predicate]
           result[:url] = sprintf(url_template, value)
+          if namespace =~ /\A(upcoming|gowalla|shizzow)\Z/
+            domain = "http://localhost:3000" if Rails.env.development? || Rails.env.test?
+            domain = "http://calagator.org" if Rails.env.production?
+            result[:url] = "#{domain}/defunct?url=#{result[:url]}"
+          end
         end
       end
 
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -4,6 +4,7 @@ describe ActsAsTaggableOn::Tag do
   describe "handling machine tags" do
     before do
       @valid_machine_tag = ActsAsTaggableOn::Tag.new(:name => 'meetup:group=1234')
+      @defunct_machine_tag = ActsAsTaggableOn::Tag.new(:name => 'upcoming:event=1234')
     end
 
     it "should return an empty hash when the tag is not a machine tag" do
@@ -18,6 +19,10 @@ describe ActsAsTaggableOn::Tag do
 
     it "should generate a url for supported namespaces/predicates" do
       @valid_machine_tag.machine_tag[:url].should eq "http://www.meetup.com/1234"
+    end
+
+    it "should redirect to 'defunct service' page with machine tag url as query param" do
+      @defunct_machine_tag.machine_tag[:url].should eq "http://localhost:3000/defunct?url=http://upcoming.yahoo.com/event/1234"
     end
   end
 end
