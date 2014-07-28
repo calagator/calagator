d2b0cbd0dc15723beff8ba32608d11af2d530ba9
Mark Lehman <markopolo@gmail.com>
2014-07-27 21:45:37 -0700

    update spec and commented example to use Meetup instead of defunct Upcoming #145

--- lib/tag_model_extensions.rb
+++ lib/tag_model_extensions.rb
@@ -60,16 +60,16 @@ module TagModelExtensions
   # key-value pairs. It may also contain an :url if one is known.
   #
   # Machine tags describe references to remote resources. For example, a
-  # Calagator event imported from an Upcoming event may have a machine
-  # linking it back to the Upcoming event.
+  # Calagator event imported from an Meetup event may have a machine
+  # linking it back to the Meetup event.
   #
   # Example:
-  #   # A tag named "upcoming:event=1234" will produce this machine tag:
+  #   # A tag named "meetup:group=1234" will produce this machine tag:
   #   tag.machine_tag == {
-  #     :namespace => "upcoming",
-  #     :predicate => "event",
+  #     :namespace => "meetup",
+  #     :predicate => "group",
   #     :value     => "1234",
-  #     :url       => "http://upcoming.yahoo.com/event/1234",
+  #     :url       => "http://www.meetup.com/1234",
   def machine_tag
     if components = self.name.match(MACHINE_TAG_PATTERN)
       namespace, predicate, value = components.captures
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe ActsAsTaggableOn::Tag do
   describe "handling machine tags" do
     before do
-      @valid_machine_tag = ActsAsTaggableOn::Tag.new(:name => 'upcoming:event=1234')
+      @valid_machine_tag = ActsAsTaggableOn::Tag.new(:name => 'meetup:group=1234')
     end
 
     it "should return an empty hash when the tag is not a machine tag" do
@@ -11,13 +11,13 @@ describe ActsAsTaggableOn::Tag do
     end
 
     it "should parse a machine tag into components" do
-      @valid_machine_tag.machine_tag[:namespace].should eq 'upcoming'
-      @valid_machine_tag.machine_tag[:predicate].should eq 'event'
+      @valid_machine_tag.machine_tag[:namespace].should eq 'meetup'
+      @valid_machine_tag.machine_tag[:predicate].should eq 'group'
       @valid_machine_tag.machine_tag[:value].should eq '1234'
     end
 
     it "should generate a url for supported namespaces/predicates" do
-      @valid_machine_tag.machine_tag[:url].should eq "http://upcoming.yahoo.com/event/1234"
+      @valid_machine_tag.machine_tag[:url].should eq "http://www.meetup.com/1234"
     end
   end
 end
