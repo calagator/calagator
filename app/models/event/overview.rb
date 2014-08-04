372f1c6f985cb5038b65ed44cafe56062cf0d7e9
Micah Geisel <micah@botandrose.com>
2014-08-04 11:24:46 -0700

    extract some private methods.

--- app/models/event/overview.rb
+++ app/models/event/overview.rb
@@ -8,10 +8,6 @@ class Event < ActiveRecord::Base
     #     :later => [...],    # Events happening within two weeks or empty array
     #     :more => ...,       # First event after the two week window or nil
     #   }
-    def times_to_events
-      @times_to_events ||= select_for_overview
-    end
-
     def today
       times_to_events[:today]
     end
@@ -34,12 +30,11 @@ class Event < ActiveRecord::Base
 
     private
 
-    def select_for_overview
-      today = Time.zone.now.beginning_of_day
-      tomorrow = today + 1.day
-      after_tomorrow = tomorrow + 1.day
-      future_cutoff = today + 2.weeks
+    def times_to_events
+      @times_to_events ||= select_for_overview
+    end
 
+    def select_for_overview
       times_to_events = {
         :today    => [],
         :tomorrow => [],
@@ -49,11 +44,11 @@ class Event < ActiveRecord::Base
 
       # Find all events between today and future_cutoff, sorted by start_time
       # includes events any part of which occurs on or after today through on or after future_cutoff
-      overview_events = Event.non_duplicates.within_dates(today, future_cutoff)
+      overview_events = Event.non_duplicates.within_dates(today_date, future_cutoff_date)
       overview_events.each do |event|
-        if event.start_time < tomorrow
+        if event.start_time < tomorrow_date
           times_to_events[:today]    << event
-        elsif event.start_time >= tomorrow && event.start_time < after_tomorrow
+        elsif event.start_time >= tomorrow_date && event.start_time < after_tomorrow_date
           times_to_events[:tomorrow] << event
         else
           times_to_events[:later]    << event
@@ -61,9 +56,25 @@ class Event < ActiveRecord::Base
       end
 
       # Find next item beyond the future_cuttoff for use in making links to it:
-      times_to_events[:more] = Event.after_date(future_cutoff).first
+      times_to_events[:more] = Event.after_date(future_cutoff_date).first
 
       times_to_events
+    end
+
+    def today_date
+      @today_date ||= Time.zone.now.beginning_of_day
+    end
+
+    def tomorrow_date
+      today_date + 1.day
+    end
+
+    def after_tomorrow_date
+      tomorrow_date + 1.day
+    end
+
+    def future_cutoff_date
+      today_date + 2.weeks
     end
   end
 end
