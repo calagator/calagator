a508545425534a12040b1860a6cf4f47c3140907
moedu23 <maureen.k.dugan@gmail.com>
2014-08-05 12:55:45 -0700

    Extract private methods to set calender entries

--- app/models/event/ical_renderer.rb
+++ app/models/event/ical_renderer.rb
@@ -4,71 +4,94 @@ class Event < ActiveRecord::Base
     attr_reader :events, :opts
 
     def initialize(events, opts)
-      @events = [events].flatten
+      @events = Array(events)
       @opts = opts
     end
 
-  	def render
-	    icalendar = RiCal.Calendar do |calendar|
-	      calendar.prodid = "-//Calagator//EN"
-	      events.each do |item|
-	        calendar.event do |entry|
-	          entry.summary(item.title || 'Untitled Event')
+    def render
+      icalendar = RiCal.Calendar do |calendar|
+        calendar.prodid = "-//Calagator//EN"
 
-	          desc = String.new.tap do |d|
-	            if item.multiday?
-	              d << "This event runs from #{TimeRange.new(item.start_time, item.end_time, :format => :text).to_s}."
-	              d << "\n\n Description:\n"
-	            end
+        events.each do |item|
+          calendar.event do |entry|
+            add_event(item, entry)
+          end
+        end
+      end
 
-	            d << Loofah::Helpers::strip_tags(item.description) if item.description.present?
-	            d << "\n\nTags: #{item.tag_list}" unless item.tag_list.blank?
-	            d << "\n\nImported from: #{opts[:url_helper].call(item)}" if opts[:url_helper]
-	          end
+      # Add the calendar name, normalize line-endings to UNIX LF, then replace them with DOS CF-LF.
+      icalendar.
+        export.
+        sub(/(CALSCALE:\w+)/i, "\\1\nX-WR-CALNAME:#{SETTINGS.name}\nMETHOD:PUBLISH").
+        gsub(/\r\n/,"\n").
+        gsub(/\n/,"\r\n")
+    end
 
-	          entry.description(desc) unless desc.blank?
+    private
 
-	          entry.created       item.created_at if item.created_at
-	          entry.last_modified item.updated_at if item.updated_at
+    def add_event(item, entry)
+      entry.summary(item.title || 'Untitled Event')
+      
+      desc = build_description(item)
+      entry.description(desc) unless desc.blank?
 
-	          # Set the iCalendar SEQUENCE, which should be increased each time an
-	          # event is updated. If an admin needs to forcefully increment the
-	          # SEQUENCE for all events, they can edit the "config/secrets.yml"
-	          # file and set the "icalendar_sequence_offset" value to something
-	          # greater than 0.
-	          entry.sequence((SECRETS.icalendar_sequence_offset || 0) + item.versions.count)
+      set_start_end(item, entry)
 
-	          if item.multiday?
-	            entry.dtstart item.dates.first
-	            entry.dtend   item.dates.last + 1.day
-	          else
-	            entry.dtstart item.start_time
-	            entry.dtend   item.end_time || item.start_time + 1.hour
-	          end
+      set_url(item, entry)
 
-	          if item.url.present?
-	            entry.url item.url
-	          end
+      set_location(item, entry)
 
-	          if item.venue
-	            entry.location [item.venue_title, item.venue.full_address].compact.join(": ")
-	          end
+      entry.created       item.created_at if item.created_at
+      entry.last_modified item.updated_at if item.updated_at
 
-	          # dtstamp and uid added because of a bug in Outlook;
-	          # Outlook 2003 will not import an .ics file unless it has DTSTAMP, UID, and METHOD
-	          # use created_at for DTSTAMP; if there's no created_at, use event.start_time;
-	          entry.dtstamp item.created_at || item.start_time
-	          entry.uid     "#{opts[:url_helper].call(item)}" if opts[:url_helper]
-	        end
-	      end
-	    end
+      # Set the iCalendar SEQUENCE, which should be increased each time an
+      # event is updated. If an admin needs to forcefully increment the
+      # SEQUENCE for all events, they can edit the "config/secrets.yml"
+      # file and set the "icalendar_sequence_offset" value to something
+      # greater than 0.
+      entry.sequence((SECRETS.icalendar_sequence_offset || 0) + item.versions.count)
 
-	    # Add the calendar name, normalize line-endings to UNIX LF, then replace them with DOS CF-LF.
-	    icalendar.
-	      export.
-	      sub(/(CALSCALE:\w+)/i, "\\1\nX-WR-CALNAME:#{SETTINGS.name}\nMETHOD:PUBLISH").
-	      gsub(/\r\n/,"\n").
-	      gsub(/\n/,"\r\n")
-	  end
-	end
+      # dtstamp and uid added because of a bug in Outlook;
+      # Outlook 2003 will not import an .ics file unless it has DTSTAMP, UID, and METHOD
+      # use created_at for DTSTAMP; if there's no created_at, use event.start_time;
+      entry.dtstamp item.created_at || item.start_time
+      entry.uid     "#{opts[:url_helper].call(item)}" if opts[:url_helper]
+    end
+
+    def build_description(item)
+      desc = ""
+      if item.multiday?
+        desc << "This event runs from #{TimeRange.new(item.start_time, item.end_time, :format => :text).to_s}."
+        desc << "\n\n Description:\n"
+      end
+
+      desc << Loofah::Helpers::strip_tags(item.description) if item.description.present?
+      desc << "\n\nTags: #{item.tag_list}" unless item.tag_list.blank?
+      desc << "\n\nImported from: #{opts[:url_helper].call(item)}" if opts[:url_helper]
+      desc
+    end
+
+    def set_start_end(item, entry)
+      if item.multiday?
+        entry.dtstart item.dates.first
+        entry.dtend   item.dates.last + 1.day
+      else
+        entry.dtstart item.start_time
+        entry.dtend   item.end_time || item.start_time + 1.hour
+      end
+    end
+
+    def set_url(item, entry)
+      if item.url.present?
+        entry.url item.url
+      end
+    end
+
+    def set_location(item, entry)
+      if item.venue
+        entry.location [item.venue_title, item.venue.full_address].compact.join(": ")
+      end
+    end
+
+  end
 end
