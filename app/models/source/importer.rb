c5221097ef27a22a916218fb70fe9482f69282e7
Micah Geisel <micah@botandrose.com>
2014-08-08 00:47:31 -0700

    minor refactor of Source::Importer.

--- app/models/source/importer.rb
+++ app/models/source/importer.rb
@@ -1,36 +1,31 @@
 class Source < ActiveRecord::Base
-  class Importer
+  class Importer < Struct.new(:source)
     def initialize params
-      @source = Source.find_or_create_from(params)
-    end
-
-    def source
-      @source
+      self.source = Source.find_or_create_from(params)
     end
 
     def valid?
-      @valid ||= @source.valid?
+      @valid ||= source.valid?
     end
 
     def events
-      if valid?
-        begin
-          return @events = @source.create_events!
-        rescue SourceParser::NotFound => e
-          @source.errors.add(:base, "No events found at remote site. Is the event identifier in the URL correct?")
-        rescue SourceParser::HttpAuthenticationRequiredError => e
-          @source.errors.add(:base, "Couldn't import events, remote site requires authentication.")
-        rescue OpenURI::HTTPError => e
-          @source.errors.add(:base, "Couldn't download events, remote site may be experiencing connectivity problems. ")
-        rescue Errno::EHOSTUNREACH => e
-          @source.errors.add(:base, "Couldn't connect to remote site.")
-        rescue SocketError => e
-          @source.errors.add(:base, "Couldn't find IP address for remote site. Is the URL correct?")
-        rescue Exception => e
-          @source.errors.add(:base, "Unknown error: #{e}")
-        end
-        nil
+      return unless valid?
+      begin
+        return source.create_events!
+      rescue SourceParser::NotFound
+        source.errors.add :base, "No events found at remote site. Is the event identifier in the URL correct?"
+      rescue SourceParser::HttpAuthenticationRequiredError
+        source.errors.add :base, "Couldn't import events, remote site requires authentication."
+      rescue OpenURI::HTTPError
+        source.errors.add :base, "Couldn't download events, remote site may be experiencing connectivity problems."
+      rescue Errno::EHOSTUNREACH
+        source.errors.add :base, "Couldn't connect to remote site."
+      rescue SocketError
+        source.errors.add :base, "Couldn't find IP address for remote site. Is the URL correct?"
+      rescue Exception => e
+        source.errors.add :base, "Unknown error: #{e}"
       end
+      nil
     end
   end
 end
