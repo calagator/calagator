e28bb73829d0a1221a03fecc109272d6fb223d17
moedu23 <maureen.k.dugan@gmail.com>
2014-08-10 16:55:05 -0700

    minor refactor.

--- app/models/event/ical_renderer.rb
+++ app/models/event/ical_renderer.rb
@@ -1,6 +1,17 @@
 class Event < ActiveRecord::Base
   class IcalRenderer
     def self.render(events, opts)
+      icalendar = build(events, opts)
+
+      # Add the calendar name, normalize line-endings to UNIX LF, then replace them with DOS CF-LF.
+      icalendar.
+        export.
+        sub(/(CALSCALE:\w+)/i, "\\1\nX-WR-CALNAME:#{SETTINGS.name}\nMETHOD:PUBLISH").
+        gsub(/\r\n/,"\n").
+        gsub(/\n/,"\r\n")
+    end
+
+    def self.build(events, opts)
       icalendar = RiCal.Calendar do |calendar|
         calendar.prodid = "-//Calagator//EN"
 
@@ -11,21 +22,14 @@ class Event < ActiveRecord::Base
           end
         end
       end
-
-      # Add the calendar name, normalize line-endings to UNIX LF, then replace them with DOS CF-LF.
-      icalendar.
-        export.
-        sub(/(CALSCALE:\w+)/i, "\\1\nX-WR-CALNAME:#{SETTINGS.name}\nMETHOD:PUBLISH").
-        gsub(/\r\n/,"\n").
-        gsub(/\n/,"\r\n")
     end
 
-    attr_reader :item, :entry, :opts
+    attr_reader :item, :entry, :imported_from
 
     def initialize(item, entry, opts)
       @item = item
       @entry = entry
-      @opts = opts
+      @imported_from = opts[:url_helper].call(item).to_s if opts[:url_helper]
     end
 
     def add_event
@@ -45,28 +49,30 @@ class Event < ActiveRecord::Base
       # SEQUENCE for all events, they can edit the "config/secrets.yml"
       # file and set the "icalendar_sequence_offset" value to something
       # greater than 0.
-      entry.sequence((SECRETS.icalendar_sequence_offset || 0) + item.versions.count)
+      entry.sequence (SECRETS.icalendar_sequence_offset || 0) + item.versions.count
 
       # dtstamp and uid added because of a bug in Outlook;
       # Outlook 2003 will not import an .ics file unless it has DTSTAMP, UID, and METHOD
       # use created_at for DTSTAMP; if there's no created_at, use event.start_time;
       entry.dtstamp item.created_at || item.start_time
-      entry.uid opts[:url_helper].call(item).to_s if opts[:url_helper]
+      entry.uid imported_from if imported_from
     end
 
     private
 
     def description
       return @desc if defined?(@desc) # memoize
+
       desc = ""
       if item.multiday?
-        desc << "This event runs from #{TimeRange.new(item.start_time, item.end_time, :format => :text).to_s}."
-        desc << "\n\n Description:\n"
+        time_range = TimeRange.new(item.start_time, item.end_time, format: :text)
+        desc << "This event runs from #{time_range}.\n\n Description:\n"
       end
 
-      desc << Loofah::Helpers::strip_tags(item.description) if item.description.present?
+      desc << Loofah::Helpers.strip_tags(item.description) if item.description.present?
       desc << "\n\nTags: #{item.tag_list}" unless item.tag_list.blank?
-      desc << "\n\nImported from: #{opts[:url_helper].call(item)}" if opts[:url_helper]
+      desc << "\n\nImported from: #{imported_from}" if imported_from
+
       @desc = desc
     end
 
