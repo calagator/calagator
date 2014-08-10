f8c31780f9b541564c1fed56a68f31c6ae9ccf18
Robb Shecter <robb@weblaws.org>
2014-08-10 16:36:27 -0700

    Test failing properly

--- spec/features/add_event_spec.rb
+++ spec/features/add_event_spec.rb
@@ -3,14 +3,14 @@ require 'factory_girl'
 
 feature "Event Creation" do
   scenario "User enters venue name", js: true do
-    new_relic = create(:venue, title: 'New Relic')
+    new_relic     = create(:venue, title: 'New Relic')
     urban_airship = create(:venue, title: 'Urban Airship')
 
     visit "/events/new"
-    fill_in "Event Name", with: 'Something'
-    fill_in "venue_name", with: "urban"
+    fill_in 'event_title', with: 'A Ruby meeting'
+    fill_in 'venue_name',  with: "urban"
 
-    expect(page).to_not have_text("New Relic")
     expect(page).to have_text("Urban Airship")
+    expect(page).to have_no_text("New Relic")
   end
 end
