eaeab0ec7a7e49e052ed5bb518f6307402ce8355
Robb Shecter <robb@weblaws.org>
2014-08-10 16:45:38 -0700

    Cleanup

--- spec/features/add_event_spec.rb
+++ spec/features/add_event_spec.rb
@@ -1,16 +1,15 @@
-require "rails_helper"
-require 'factory_girl'
+require 'rails_helper'
 
-feature "Event Creation" do
-  scenario "User enters venue name", js: true do
-    new_relic     = create(:venue, title: 'New Relic')
-    urban_airship = create(:venue, title: 'Urban Airship')
+feature 'Event Creation' do
+  scenario 'User begins typing a venue name', js: true do
+    create(:venue, title: 'New Relic')
+    create(:venue, title: 'Urban Airship')
 
-    visit "/events/new"
+    visit '/events/new'
     fill_in 'event_title', with: 'A Ruby meeting'
-    fill_in 'venue_name',  with: "urban"
+    fill_in 'venue_name',  with: 'urban'
 
-    expect(page).to have_text("Urban Airship")
-    expect(page).to have_no_text("New Relic")
+    expect(page).to have_text('Urban Airship')
+    expect(page).to have_no_text('New Relic')
   end
 end
