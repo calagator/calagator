52efa50d74c46f63cc6aa70231309c67620c744a
Micah Geisel <micah@botandrose.com>
2014-08-12 00:46:18 -0700

    extract more private methods out of Event::IcalRenderer#add_event.

--- app/models/event/ical_renderer.rb
+++ app/models/event/ical_renderer.rb
@@ -33,32 +33,24 @@ class Event < ActiveRecord::Base
     end
 
     def add_event
-      entry.summary event.title || 'Untitled Event'
-
-      entry.description description unless description.blank?
-      entry.url event.url if event.url.present?
+      entry.summary summary
+      entry.description description if description
+      entry.url url if url
       entry.location location if location
-      entry.dtstart start_time
-      entry.dtend end_time
-
-      entry.created       event.created_at if event.created_at
-      entry.last_modified event.updated_at if event.updated_at
-
-      # Set the iCalendar SEQUENCE, which should be increased each time an
-      # event is updated. If an admin needs to forcefully increment the
-      # SEQUENCE for all events, they can edit the "config/secrets.yml"
-      # file and set the "icalendar_sequence_offset" value to something
-      # greater than 0.
-      entry.sequence (SECRETS.icalendar_sequence_offset || 0) + event.versions.count
-
-      # dtstamp and uid added because of a bug in Outlook;
-      # Outlook 2003 will not import an .ics file unless it has DTSTAMP, UID, and METHOD
-      # use created_at for DTSTAMP; if there's no created_at, use event.start_time;
-      entry.dtstamp event.created_at || event.start_time
-      entry.uid imported_from if imported_from
+      entry.dtstart dtstart
+      entry.dtend dtend
+      entry.created created if created
+      entry.last_modified last_modified if last_modified
+      entry.sequence sequence
+      entry.dtstamp dtstamp
+      entry.uid uid if uid
     end
 
     private
+
+    def summary
+      event.title || 'Untitled Event'
+    end
 
     def description
       return @desc if defined?(@desc) # memoize
@@ -73,14 +65,18 @@ class Event < ActiveRecord::Base
       desc << "\n\nTags: #{event.tag_list}" unless event.tag_list.blank?
       desc << "\n\nImported from: #{imported_from}" if imported_from
 
-      @desc = desc
+      @desc = desc.present? ? desc : nil
+    end
+
+    def url
+      event.url if event.url.present?
     end
 
     def location
       [event.venue_title, event.venue.full_address].compact.join(": ") if event.venue
     end
 
-    def start_time
+    def dtstart
       if event.multiday?
         event.dates.first
       else
@@ -88,12 +84,40 @@ class Event < ActiveRecord::Base
       end
     end
 
-    def end_time
+    def dtend
       if event.multiday?
         event.dates.last + 1.day
       else
         event.end_time || event.start_time + 1.hour
       end
+    end
+
+    def created
+      event.created_at if event.created_at
+    end
+
+    def last_modified
+      event.updated_at if event.updated_at
+    end
+
+    def sequence
+      # Set the iCalendar SEQUENCE, which should be increased each time an
+      # event is updated. If an admin needs to forcefully increment the
+      # SEQUENCE for all events, they can edit the "config/secrets.yml"
+      # file and set the "icalendar_sequence_offset" value to something
+      # greater than 0.
+      (SECRETS.icalendar_sequence_offset || 0) + event.versions.count
+    end
+
+    def dtstamp
+      # dtstamp and uid added because of a bug in Outlook;
+      # Outlook 2003 will not import an .ics file unless it has DTSTAMP, UID, and METHOD
+      # use created_at for DTSTAMP; if there's no created_at, use event.start_time;
+      event.created_at || event.start_time
+    end
+
+    def uid
+      imported_from
     end
   end
 end
