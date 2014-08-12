a2ad90f325e2ce1fac7e259b578c62ea12e14d91
Reid Beels <mail@reidbeels.com>
2014-08-12 10:11:35 -0700

    Remove JS event triggers from Venue autocomplete spec

--- spec/features/add_event_spec.rb
+++ spec/features/add_event_spec.rb
@@ -2,17 +2,11 @@ require 'rails_helper'
 
 feature 'Event Creation' do
   scenario 'User begins typing a venue name', js: true do
-    create(:venue, title: 'New Relic')
-    create(:venue, title: 'Urban Airship')
+    create :venue, title: 'New Relic'
+    create :venue, title: 'Urban Airship'
 
     visit '/events/new'
-    fill_in 'event_title', with: 'A Ruby meeting'
-    fill_in 'venue_name',  with: 'urban'
-
-    page.execute_script %Q{ $('#venue_name').trigger('focus') }
-    page.execute_script %Q{ $('#venue_name').trigger('keydown') }
-
-    wait_for_ajax
+    find_field('Venue').native.send_keys 'urban'
 
     expect(page).to have_text('Urban Airship')
     expect(page).to have_no_text('New Relic')
