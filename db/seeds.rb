b2164ec214e7d9a7b88a80793abc8ff0f6533ce5
Natalie <blackburn87@gmail.com>
2014-08-23 20:11:04 -0700

    add event seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,17 @@
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
 require 'faker'
+require 'active_support/core_ext'
 
-def time_rand
-  from = 0.0, to = Time.now
-  Time.at(from + rand * (to.to_f - from.to_f))
+def rand_date
+  date = DateTime.current
+  date.change(:year => (date.year + rand(-2..2)), 
+              :month => rand(1..12), 
+              :day => rand(1..28), 
+              :hour => rand(1..24))
 end
 
-100.times do | num |
+100.times do
   mock_loc = Faker::Address
   mock_web = Faker::Internet
   mock_num = Faker::Number
@@ -23,9 +27,9 @@ end
   Venue.create!(
     :title           => Faker::Company.name,
     :address         => "#{mock_loc.street_address},
-                           #{mock_loc.city},
-                           #{mock_loc.state}
-                           #{mock_loc.zip_code}",
+                         #{mock_loc.city},
+                         #{mock_loc.state}
+                         #{mock_loc.zip_code}",
     :url             => mock_web.url,
     :street_address  => mock_loc.street_address,
     :locality        => mock_loc.city,
@@ -38,19 +42,24 @@ end
     :telephone       => Faker::PhoneNumber.phone_number,
     :closed          => [true, false].sample,
     :wifi            => [true, false].sample,
-    :access_notes    => Faker::Lorem.paragraph(2),
+    :access_notes    => Faker::Lorem.paragraph,
     )
 end
 
-# Fields needed for Events seed data
-# 100.times do
-#   t.string   "title"
-#   t.text     "description"
-#   t.datetime "start_time"
-#   t.integer  "venue_id" load all venue ids and pick one
-#   t.string   "url"
-#   t.datetime "created_at",  event start time minus one day
-#   t.datetime "updated_at",  event start time minus one day
-#   t.datetime "end_time"
-#   t.text     "venue_details"
-# end
+venue_ids = Venue.all(:select => :id).collect(&:id)
+
+1000.times do
+  event_date = rand_date
+  selected_id = venue_ids[rand(0..venue_ids.length)]
+
+  Event.create!(
+  :title          => Faker::Lorem.sentence,
+  :description    => Faker::Lorem.paragraph,
+  :start_time     => event_date,
+  :venue_id       => selected_id,
+  :url            => Faker::Internet.url,
+  :created_at     => event_date - 1.days,
+  :end_time       => event_date + 3.hours,
+  :venue_details  => Faker::Lorem.paragraph
+  )
+end
