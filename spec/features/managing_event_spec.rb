2748f44ae046ab712b4f256cfe83a51d751a62c9
moedu23 <maureen.k.dugan@gmail.com>
2014-10-26 17:41:17 -0700

    Test cloning an event

--- spec/features/managing_event_spec.rb
+++ spec/features/managing_event_spec.rb
@@ -44,3 +44,46 @@ feature 'Event Editing' do
     end
   end
 end
+
+feature 'Event Cloning' do
+  background do
+    Timecop.travel('2014-10-09')
+    create :event, title: 'Ruby Event Part One', start_time: today + 4.day
+  end
+
+  after do
+    Timecop.return
+  end
+
+  scenario 'A user clones an existing event' do
+    visit '/'
+
+    within '#next_two_weeks' do
+      click_on 'Ruby Event Part One'
+    end
+    click_on 'clone'
+
+    find_field('Event Name').value.should have_content 'Ruby Event Part One'
+
+    fill_in 'Event Name', with: 'Ruby Event Part Two'
+    fill_in 'start_date', with: '2014-10-27'
+    fill_in 'start_time', with: '06:00 PM'
+    fill_in 'end_time', with: '11:00 PM'
+    fill_in 'end_date', with: '2014-27-13'
+    fill_in 'Website', with: 'www.rubynewbies.com'
+    fill_in 'Description', with: 'An event for beginners'
+    fill_in 'Tags', with: 'beginners,ruby'
+    click_on 'Create Event'
+
+    page.should have_content 'Event was successfully saved'
+    page.should have_content 'Ruby Event Part Two'
+    page.should have_content 'Monday, October 27, 2014 at 6pm'
+    page.should have_content 'Website http://www.rubynewbies.com'
+    page.should have_content 'Description An event for beginners'
+    page.should have_content 'Tags beginners, ruby'
+
+    click_on 'Calagator'
+    click_on 'View future events »'
+    page.should have_content 'Ruby Event Part Two'
+  end
+end
