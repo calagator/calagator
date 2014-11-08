13b26b26d50558722b37a800aecb1f910333964d
Natalie Blackburn <blackburn87@gmail.com>
2014-11-08 15:49:52 -0800

    ensure checkbox checking functions properly

--- spec/features/add_venue_spec.rb
+++ spec/features/add_venue_spec.rb
@@ -10,15 +10,13 @@ feature 'Venue Creation' do
     fill_in 'Full Address', with: '350 5th Ave, New York, NY 10118, US'
     fill_in 'Web Link', with: 'EmpireStateBuildi.ng'
     fill_in 'Email', with: 'empire@state_building.com'
-    fill_in 'Telephone', with: '298-943-1337'
-    check 'venue_wifi'
+    fill_in 'Telephone', with: '(298)943-1337'
+    check("venue_wifi")
     fill_in 'Description', with: 'Famous New York City Skyscraper'
     fill_in 'Access notes', with: 'Just knock I guess?'
     # fill_in 'Tags', with: ??
 
     click_on 'Create Venue'
-
-    checkbox = find('#venue_wifi')
 
     page.should have_content 'Venue was successfully saved.'
     page.should have_content 'Empire State Building'
@@ -26,7 +24,7 @@ feature 'Venue Creation' do
     page.should have_content 'EmpireStateBuildi.ng'
     page.should have_content 'empire@state_building.com'
     page.should have_content '(298)943-1337'
-    checkbox.should be_checked
+    page.should have_content 'Public WiFi'
     page.should have_content 'Famous New York City Skyscraper'
     page.should have_content 'Just knock I guess?'
   end
