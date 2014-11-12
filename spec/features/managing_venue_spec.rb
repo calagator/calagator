7f2ecd124259ff142d754fd5bfd1258d484d2817
Natalie <blackburn87@gmail.com>
2014-11-12 10:56:31 -0800

    convert to test harness using venue factory

--- spec/features/managing_venue_spec.rb
+++ spec/features/managing_venue_spec.rb
@@ -2,41 +2,48 @@ require 'rails_helper'
 
 feature 'Venue Editing' do
   let(:venue) { create(:venue) }
+  let(:new_venue) { build(:venue) }
 
   scenario 'A user edits an existing venue' do
 
     visit "/venues/#{venue.id}"
     click_on 'edit'
 
-    find_field('Venue Name').value.should have_content "#{venue.title}"
-    fill_in 'Venue Name', with: 'Space Needle'
-    fill_in 'Street address', with: '400 Broad Street'
-    fill_in 'City', with: 'Seattle'
-    fill_in 'State', with: 'WA'
-    fill_in 'Zip Code', with: '98109'
-    fill_in 'Country', with: 'US'
+    venue_name = find_field('Venue Name').value
+    expect(venue_name).to have_content "#{venue.title}"
+
+    fill_in 'Venue Name', with: new_venue.title
+    fill_in 'Street address', with: new_venue.street_address
+    fill_in 'City', with: new_venue.locality
+    fill_in 'State', with: new_venue.region
+    fill_in 'Zip Code', with: new_venue.postal_code
+    fill_in 'Country', with: new_venue.country
     # fill_in 'Latitude*', with: ??
     # fill_in 'Longitude*', with: ??
-    fill_in 'Web Link', with: 'SpaceNeed.le'
-    fill_in 'Email', with: 'SpaceNeedle@hotmail.com'
-    fill_in 'Telephone', with: '(298)587-7825'
-    check('venue_wifi')
-    fill_in 'Description', with: 'Most famous pointy building in Seattle.'
-    fill_in 'Access notes', with: 'Just pay the exorbitant ticket price.'
-    check('venue_closed')
+    fill_in 'Web Link', with: new_venue.url
+    fill_in 'Email', with: new_venue.email
+    fill_in 'Telephone', with: new_venue.telephone
+    check('venue_wifi') if new_venue.wifi
+    fill_in 'Description', with: new_venue.description
+    fill_in 'Access notes', with: 'Just pay the ticket price.'
+    check('venue_closed') if new_venue.closed
     # check('venue_force_geocoding')
 
     click_on 'Update Venue'
 
     expect(page).to have_content 'Venue was successfully saved.'
-    expect(page).to have_content 'Space Needle'
-    expect(page).to have_content '400 Broad Street Seattle, WA 98109, US'
-    expect(page).to have_content 'SpaceNeed.le'
-    expect(page).to have_content 'SpaceNeedle@hotmail.com'
-    expect(page).to have_content '(298)587-7825'
+    expect(page).to have_content new_venue.title
+    expect(page).to have_content new_venue.street_address
+    expect(page).to have_content new_venue.locality
+    expect(page).to have_content new_venue.region
+    expect(page).to have_content new_venue.postal_code
+    expect(page).to have_content new_venue.country
+    expect(page).to have_content new_venue.url
+    expect(page).to have_content new_venue.email
+    expect(page).to have_content new_venue.telephone
+    expect(page).to have_content new_venue.description
     expect(page).to have_content 'Public WiFi'
-    expect(page).to have_content 'Most famous pointy building in Seattle.'
-    expect(page).to have_content 'Just pay the exorbitant ticket price.'
-    expect(page).to have_content 'This venue is no longer open for business.'
+    expect(page).to have_content 'Just pay the ticket price.'
+    expect(page).to have_content 'This venue is no longer open for business.' if new_venue.closed
   end
 end
