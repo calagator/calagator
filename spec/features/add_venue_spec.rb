1795043d1ff9f2932dc74e45efdd971ea89d62cb
Natalie <blackburn87@gmail.com>
2014-11-12 16:13:00 -0800

    add harness to add_venue_spec

--- spec/features/add_venue_spec.rb
+++ spec/features/add_venue_spec.rb
@@ -1,31 +1,33 @@
 require 'rails_helper'
 
 feature 'Venue Creation' do
+  let(:new_venue) { build(:venue) }
+
   scenario 'User adds a new venue' do
 
     visit '/venues'
     click_on 'Add a venue'
 
-    fill_in 'Venue Name', with: 'Empire State Building'
-    fill_in 'Full Address', with: '350 5th Ave, New York, NY 10118, US'
-    fill_in 'Web Link', with: 'EmpireStateBuildi.ng'
-    fill_in 'Email', with: 'empire@state_building.com'
-    fill_in 'Telephone', with: '(298)943-1337'
+
+    fill_in 'Venue Name', with: new_venue.title
+    fill_in 'Full Address', with: new_venue.address
+    fill_in 'Web Link', with: new_venue.url
+    fill_in 'Email', with: new_venue.email
+    fill_in 'Telephone', with: new_venue.telephone
     check("venue_wifi")
-    fill_in 'Description', with: 'Famous New York City Skyscraper'
-    fill_in 'Access notes', with: 'Just knock I guess?'
-    # fill_in 'Tags', with: ??
+    fill_in 'Description', with: new_venue.description
+    fill_in 'Access notes', with: 'Just knock?'
 
     click_on 'Create Venue'
 
     expect(page).to have_content 'Venue was successfully saved.'
-    expect(page).to have_content 'Empire State Building'
-    expect(page).to have_content '350 5th Ave, New York, NY 10118, US'
-    expect(page).to have_content 'EmpireStateBuildi.ng'
-    expect(page).to have_content 'empire@state_building.com'
-    expect(page).to have_content '(298)943-1337'
+    expect(page).to have_content new_venue.title
+    expect(page).to have_content new_venue.address
+    expect(page).to have_content new_venue.url
+    expect(page).to have_content new_venue.email
+    expect(page).to have_content new_venue.telephone
+    expect(page).to have_content new_venue.description
+    expect(page).to have_content 'Just knock?'
     expect(page).to have_content 'Public WiFi'
-    expect(page).to have_content 'Famous New York City Skyscraper'
-    expect(page).to have_content 'Just knock I guess?'
   end
 end
