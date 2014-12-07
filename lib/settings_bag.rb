c06b9ca16ca79b28c3c08e0423614ea445dce180
Maureen Dugan <maureen.k.dugan@gmail.com>
2014-12-06 17:07:19 -0800

    Add notes on loading YAML from ENV
    Not an ideal solution, but one to consider so we can match
    the functionality that is currently represented.
    
    - @jc00ke

--- lib/settings_bag.rb
+++ lib/settings_bag.rb
@@ -22,3 +22,16 @@ s = SettingsBag.new({foo: 1})
 s.foo # => 1
 s.bar(2) -> s.method_missing(:bar, 2)
 s.respond_to?(:foo) # => true
+
+
+def method_missing(meth, *args, &block)
+  @data.fetch(meth, get_env_var(meth))
+end
+
+
+private
+def get_env_var(var)
+  if v = ENV[var.to_s.upcase]
+    YAML.load(v)
+  end
+end
