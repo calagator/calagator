4dbf74b1df6ba802f56f4b3ee18b98f7f58412f6
Micah Geisel <micah@botandrose.com>
2014-12-08 01:08:06 -0800

    extract some private methods in Venue::Geocoder.

--- app/models/venue/geocoder.rb
+++ app/models/venue/geocoder.rb
@@ -1,12 +1,12 @@
 class Venue < ActiveRecord::Base
-  class Geocoder < Struct.new(:venue)
+  class Geocoder < Struct.new(:venue, :geo)
     def self.geocode(venue)
       new(venue).geocode
     end
 
     def geocode
-      if Venue.perform_geocoding? && venue.location.blank? && venue.geocode_address.present? && venue.duplicate_of.blank?
-        geo = GeoKit::Geocoders::MultiGeocoder.geocode(venue.geocode_address)
+      if should_geocode?
+        self.geo = GeoKit::Geocoders::MultiGeocoder.geocode(venue.geocode_address)
         if geo.success
           venue.latitude       = geo.lat
           venue.longitude      = geo.lng
@@ -16,12 +16,28 @@ class Venue < ActiveRecord::Base
           venue.postal_code    = geo.zip            if venue.postal_code.blank?
           venue.country        = geo.country_code   if venue.country.blank?
         end
-
-        msg = 'Venue#add_geocoding for ' + (venue.new_record? ? 'new record' : "record #{venue.id}") + ' ' + (geo.success ? 'was successful' : 'failed') + ', response was: ' + geo.inspect
-        Rails.logger.info(msg)
+        log
       end
 
       return true
+    end
+
+    private
+
+    def should_geocode?
+      [
+        Venue.perform_geocoding?,
+        venue.location.blank?,
+        venue.geocode_address.present?,
+        venue.duplicate_of.blank?
+      ].all?
+    end
+
+    def log
+      venue_id = venue.new_record? ? "new record" : "record #{venue.id}"
+      status = geo.success ? "was successful" : "failed"
+      message = "Venue#add_geocoding for #{venue} #{status}, response was: #{geo.inspect}"
+      Rails.logger.info message
     end
   end
 end
