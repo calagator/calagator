a0be31d8a0c0f571a1a627e80d6fda474db10bb2
Micah Geisel <micah@botandrose.com>
2014-12-08 21:08:43 -0800

    rm usage of abstract items in SourceParser::Meetup.

--- app/models/source_parser/meetup.rb
+++ app/models/source_parser/meetup.rb
@@ -3,7 +3,7 @@ class SourceParser # :nodoc:
     label :Meetup
     url_pattern %r{^http://(?:www\.)?meetup\.com/[^/]+/events/([^/]+)/?}
 
-    def self.to_abstract_events(opts={})
+    def self.to_events(opts={})
       if SECRETS.meetup_api_key.present?
         self.to_abstract_events_api_helper(
           :url => opts[:url],
@@ -20,16 +20,17 @@ class SourceParser # :nodoc:
             ]
           }
         ) do |data, event_id|
-          event = AbstractEvent.new
+          event = Event.new
+          event.source      = opts[:source]
           event.title       = data['name']
           event.description = data['description']
           # Meetup sends us milliseconds since the epoch in UTC
           event.start_time  = Time.at(data['time']/1000).utc
           event.url         = data['event_url']
-          event.location    = to_abstract_location(data['venue'])
-          event.tags        = ["meetup:event=#{event_id}", "meetup:group=#{data['group']['urlname']}"]
+          event.venue       = to_venue(data['venue'])
+          event.tag_list    = "meetup:event=#{event_id}, meetup:group=#{data['group']['urlname']}"
 
-          [event]
+          [event_or_duplicate(event)]
         end
       else
         self.to_abstract_events_wrapper(
@@ -41,19 +42,21 @@ class SourceParser # :nodoc:
       end
     end
 
-    def self.to_abstract_location(value, opts={})
-      if value.present?
-        location = AbstractLocation.new
-        location.title   = value['name']
-        location.street_address = [value['address_1'], value['address_2'], value['address_3']].compact.join(", ")
-        location.locality = value['city']
-        location.region = value['state']
-        location.postal_code = value['zip']
-        location.country = value['country']
-        location.telephone = value['phone']
-        location.tags = ["meetup:venue=#{value['id']}"]
-        location
-      end
+    def self.to_venue(value, opts={})
+      return if value.blank?
+      venue = Venue.new({
+        source: opts[:source],
+        title: value['name'],
+        street_address: [value['address_1'], value['address_2'], value['address_3']].compact.join(", "),
+        locality: value['city'],
+        region: value['state'],
+        postal_code: value['zip'],
+        country: value['country'],
+        telephone: value['phone'],
+        tag_list: "meetup:venue=#{value['id']}",
+      })
+      venue.geocode!
+      venue_or_duplicate(venue)
     end
   end
 end
