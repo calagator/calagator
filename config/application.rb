5ab3a44b229759be3361459c9522d3232bdc2560
Reid Beels <mail@reidbeels.com>
2014-12-29 22:27:11 -0800

    Vendor a copy of leaflet to avoid offline spec failures

--- config/application.rb
+++ config/application.rb
@@ -48,7 +48,11 @@ module Calagator
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.precompile += ["leaflet_google_layer.js", "errors.css"]
+    config.assets.precompile += [
+      "leaflet.js",
+      "leaflet_google_layer.js",
+      "errors.css"
+    ]
 
     #---[ Rack Middleware ]-------------------------------------------------
 
