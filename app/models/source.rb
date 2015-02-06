a3cb0a4beb1d60555f764ebc9711341f0068d02e
Micah Geisel <micah@botandrose.com>
2015-02-06 01:00:54 -0800

    remove unused Update model.

--- app/models/source.rb
+++ app/models/source.rb
@@ -20,7 +20,6 @@ class Source < ActiveRecord::Base
 
   has_many :events,  :dependent => :destroy
   has_many :venues,  :dependent => :destroy
-  has_many :updates, :dependent => :destroy
 
   scope :listing, -> { order('created_at DESC') }
 
