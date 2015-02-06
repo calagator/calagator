54d3c48c8b8d9bc5af0d50b8418cbee07d434b6c
Micah Geisel <micah@botandrose.com>
2015-02-06 00:05:00 -0800

    spec/features should test dummy app in spec/dummy.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
-require File.expand_path("../../config/environment", __FILE__)
+require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
