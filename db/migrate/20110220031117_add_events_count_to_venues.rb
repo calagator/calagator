0bdede19580c223980ad83c09e4939d03e6bbdb2
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-02-07 15:11:27 -0800

    fix syntax error in migration add_events_count_to_venue

--- db/migrate/20110220031117_add_events_count_to_venues.rb
+++ db/migrate/20110220031117_add_events_count_to_venues.rb
@@ -1,6 +1,7 @@
 class AddEventsCountToVenues < ActiveRecord::Migration
   def self.up
     add_column :venues, :events_count, :integer
+  end
   def self.down
     remove_column :venues, :events_count
   end
