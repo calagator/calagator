4674eda0e27cff3b56b5862858b99bdd8312b25f
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-02-07 14:31:29 -0800

    fixed migration namespacing for activerecord models

--- db/migrate/20080604210521_convert_times_to_utc.rb
+++ db/migrate/20080604210521_convert_times_to_utc.rb
@@ -1,6 +1,6 @@
 class ConvertTimesToUtc < ActiveRecord::Migration
   def self.up
-    Event.all.each do |e|
+    Calagator::Event.all.each do |e|
       e.start_time = Time.parse(e.start_time_before_type_cast)
       e.save
     end
