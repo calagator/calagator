2b74d0729a33b27352e9c8fd87f32a5e1dd56bf5
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-02-07 14:44:52 -0800

    remove unneeded data massaging from migrations so they run from scratch

--- db/migrate/20110220031117_add_events_count_to_venues.rb
+++ db/migrate/20110220031117_add_events_count_to_venues.rb
@@ -1,13 +1,6 @@
 class AddEventsCountToVenues < ActiveRecord::Migration
   def self.up
     add_column :venues, :events_count, :integer
-
-    Calagator::Venue.reset_column_information
-    Calagator::Venue.all.each do |p|
-      Calagator::Venue.update_counters p.id, :events_count => p.events.length
-    end
-  end
-
   def self.down
     remove_column :venues, :events_count
   end
