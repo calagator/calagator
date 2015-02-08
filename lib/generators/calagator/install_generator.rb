7256db2c03f47dae962832bc6c9b9084893e1a66
James Kurczodyna <james@wearefine.com>
2015-02-08 14:50:18 -0800

    update install_generator to setup initializer

--- lib/generators/calagator/install_generator.rb
+++ lib/generators/calagator/install_generator.rb
@@ -6,6 +6,7 @@ module Calagator
       run 'bundle install'
       add_route
       add_secrets
+      add_initializer
       rake 'calagator:install:migrations'
       rake 'db:migrate'
     end
@@ -13,11 +14,15 @@ module Calagator
     private
 
     def add_route
-      inject_into_file 'config/routes.rb', "mount Calagator::Engine => '/'\n", after: "Application.routes.draw do\n"
+      inject_into_file 'config/routes.rb', "\tmount Calagator::Engine => '/'\n", after: "Application.routes.draw do\n"
     end
 
     def add_secrets
-      copy_file File.expand_path(File.join(__FILE__, '../templates/secrets.yml.sample')), 'config/secrets.yml'
+      copy_file File.expand_path(File.join(__FILE__, '../templates/config/secrets.yml.sample')), 'config/secrets.yml'
+    end
+
+    def add_initializer
+      copy_file File.expand_path(File.join(__FILE__, '../templates/config/calagator.rb')), 'config/initializers/calagator.rb'
     end
 
   end
---
+++ lib/generators/calagator/templates/config/calagator.rb
@@ -0,0 +1,21 @@
+Calagator.setup do |config|
+
+  # # Site's name
+  # config.title = 'Calagator'
+
+  # # Site's tagline
+  # config.tagline = 'A Tech Calendar'
+
+  # # Site's URL with trailing slash
+  # config.url = 'http://my-calagator.org/'
+
+  # # Map to display on /venues page:
+  # config.venues_map_options = {
+  #   # Zoom magnification level:
+  #   zoom: 12,
+  #   # Center of the map, in latitude and longitude.
+  #   # If no center is specified, the map will zoom to fit all markers.
+  #   center: [45.518493, -122.660737]
+  # }
+
+end
