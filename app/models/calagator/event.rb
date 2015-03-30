0894d0ee6a73cda0026461833575fd0eaba0541e
Micah Geisel <micah@botandrose.com>
2015-03-30 13:20:13 -0700

    assigning a date should assume current timezone, not UTC.

--- app/models/calagator/event.rb
+++ app/models/calagator/event.rb
@@ -144,6 +144,7 @@ class Event < ActiveRecord::Base
 
   def time_for(value)
     value = value.join(' ') if value.kind_of?(Array)
+    value = value.to_s if value.kind_of?(Date)
     value = Time.zone.parse(value) if value.kind_of?(String) # this will throw ArgumentError if invalid
     value
   end
