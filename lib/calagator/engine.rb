955f0883d69106a7eca3a167e1c58456547be8b7
Audrey Eschright <spinnerin@gmail.com>
2015-05-16 15:44:29 -0700

    Adding leaflet assets to the engine's precompile list to fix #364

--- lib/calagator/engine.rb
+++ lib/calagator/engine.rb
@@ -1,5 +1,9 @@
 module Calagator
   class Engine < ::Rails::Engine
     isolate_namespace Calagator
+
+    initializer "calagator.assets.precompile" do |app|
+      app.config.assets.precompile += %w( markers-soft.png markers-shadow.png markers-soft@2x.png markers-shadow@2x.png leaflet.js )
+    end
   end
 end
