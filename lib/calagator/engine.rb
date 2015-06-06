82f3c736d750953a7fae0dbadb5ad0e3a37976fe
Reid Beels <mail@reidbeels.com>
2015-06-05 23:09:11 -0700

    Precompile leaflet_google_layer.js

--- lib/calagator/engine.rb
+++ lib/calagator/engine.rb
@@ -3,7 +3,16 @@ module Calagator
     isolate_namespace Calagator
 
     initializer "calagator.assets.precompile" do |app|
-      app.config.assets.precompile += %w( markers-soft.png markers-shadow.png markers-soft@2x.png markers-shadow@2x.png leaflet.js mustache.js )
+      app.config.assets.precompile += %w( 
+        markers-soft.png
+        markers-shadow.png
+        markers-soft@2x.png
+        markers-shadow@2x.png
+        leaflet.js
+        leaflet_google_layer.js
+        mustache.js
+        leaflet
+      )
     end
   end
 end
