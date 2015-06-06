49a055b7d3abb8cc39f2e28f8c35cea752059aa5
Reid Beels <mail@reidbeels.com>
2015-06-05 23:23:42 -0700

    Fix config of engine asset precompilation

--- lib/calagator/engine.rb
+++ lib/calagator/engine.rb
@@ -2,17 +2,15 @@ module Calagator
   class Engine < ::Rails::Engine
     isolate_namespace Calagator
 
-    initializer "calagator.assets.precompile" do |app|
-      app.config.assets.precompile += %w( 
-        markers-soft.png
-        markers-shadow.png
-        markers-soft@2x.png
-        markers-shadow@2x.png
-        leaflet.js
-        leaflet_google_layer.js
-        mustache.js
-        leaflet
-      )
-    end
+    config.assets.precompile += %w( 
+      markers-soft.png
+      markers-shadow.png
+      markers-soft@2x.png
+      markers-shadow@2x.png
+      leaflet.js
+      leaflet_google_layer.js
+      mustache.js
+      leaflet
+    )
   end
 end
