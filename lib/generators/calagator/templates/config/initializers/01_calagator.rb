c7872d1ec787868b230664ea9f2f0b4fc6063bf3
Reid Beels <mail@reidbeels.com>
2015-06-08 01:14:12 -0700

    Uncomment defaults in Calagator config initializer

--- lib/generators/calagator/templates/config/initializers/01_calagator.rb
+++ lib/generators/calagator/templates/config/initializers/01_calagator.rb
@@ -1,51 +1,51 @@
 Calagator.setup do |config|
   # Site name
-  # config.title = 'Calagator'
+  config.title = 'Calagator'
 
   # Site tagline
-  # config.tagline = 'A Tech Calendar'
+  config.tagline = 'A Tech Calendar'
 
   # Site URL with trailing slash
-  # config.url = 'http://my-calagator.org/'
+  config.url = 'http://my-calagator.org/'
 
   # Email address of administrator that will get exception notifications
   # and requests for assistance from users:
-  # config.administrator_email = 'your@email.addr'
+  config.administrator_email = 'your@email.addr'
 
-  # Admin username and password to use when accessing /admin pages
+  # Optional username and password to use when accessing /admin pages
   # config.admin_username = 'admin'
   # config.admin_password = 'kitties!'
 
   # API key for Meetup.com, get your own from: http://www.meetup.com/meetup_api/key/
-  # config.meetup_api_key = '<your meetup API key>'
+  config.meetup_api_key = ''
 
   # Search engine to use for searching events.
-  # Values: :sql, :sunspot. Defaults to :sql.
-  # config.search_engine = :sql
+  # Values: :sql, :sunspot.
+  config.search_engine = :sql
 
   # Set the iCalendar SEQUENCE, which should be increased each time an event
   # is updated. If an admin needs to forcefully increment the SEQUENCE for all
   # events, they can set this icalendar_sequence_offset value to something
   # greater than 0.
-  # config.icalendar_sequence_offset = 0
+  config.icalendar_sequence_offset = 0
 
   # Configure a mapping provider
   # Stamen's terrain tiles will be used by default.
   # Map marker color
   # Values: red, darkred, orange, green, darkgreen, blue, purple, darkpuple, cadetblue
-  # config.mapping_marker_color = 'green'
+  config.mapping_marker_color = 'green'
 
   # A Google Maps API key is required to use Google's geocoding service
   # as well as to display maps using their API.
   # Get one at: https://developers.google.com/maps/documentation/javascript/tutorial#api_key
-  # config.mapping_google_maps_api_key = '<your google maps v3 API key>'
+  config.mapping_google_maps_api_key = ''
 
   # The tile provider to use when rendering maps with Leaflet.
   # One of: leaflet, stamen, mapbox, google
-  # config.mapping_provider = 'stamen'
+  config.mapping_provider = 'stamen'
 
   # The tiles to use for the map, see the docs for individual Leaflet plugins.
-  # config.mapping_tiles = 'terrain'
+  config.mapping_tiles = 'terrain'
 
   # Other mapping examples:
   #
@@ -72,18 +72,19 @@ Calagator.setup do |config|
   # config.mapping_tiles = 'Gray'
 
   # Map to display on /venues page:
-  # config.venues_map_options = {
-  #   # Zoom magnification level:
-  #   zoom: 12,
-  #   # Center of the map, in latitude and longitude.
-  #   # If no center is specified, the map will zoom to fit all markers.
-  #   center: [45.518493, -122.660737]
-  # }
+  config.venues_map_options = {
+    # Zoom magnification level:
+    zoom: 12,
+
+    # Center of the map, in latitude and longitude.
+    # If no center is specified, the map will zoom to fit all markers.
+    # center: [45.518493, -122.660737]
+  }
 
   # Patterns for detecting spam events and venues
-  # config.blacklist_patterns = [
-  #   /\b(online|overseas).+(drugstore|pharmacy)\b/,
-  #   /\bcialis\b/,
-  # ]
+  config.blacklist_patterns = [
+    /\b(online|overseas).+(drugstore|pharmacy)\b/,
+    /\bcialis\b/,
+  ]
 
 end
