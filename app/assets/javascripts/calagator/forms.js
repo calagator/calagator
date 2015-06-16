2bc4b6930c94aa07b10807d49ab3caac56920634
Jon McDuffie <bobber321988@gmail.com>
2015-06-16 11:20:33 -0400

    Refactor js to work with turbolinks

--- app/assets/javascripts/calagator/forms.js
+++ app/assets/javascripts/calagator/forms.js
@@ -1,7 +1,7 @@
 //= require jquery-ui
 //= require jquery.timepicker
 
-$(document).ready(function(){
+$(document).on('page:change',function(){
   // Initialize autocompletion for venues
   $("input.autocomplete").each(function() {
     $(this).attr('autocomplete', 'off').autocomplete({
@@ -31,30 +31,32 @@ $(document).ready(function(){
   $('.date_picker').datepicker({ dateFormat: 'yy-mm-dd' });
   $('.time_picker').timepicker({ 'timeFormat': 'g:i A' });
 
-  // Update "time_end" to maintain the same offset from "time_start" if "time_start" changes, and display a highlight to alert the user.
+  // Set oldTime to allow time offset functionality
   var oldTime = $("#time_start").timepicker('getTime');
-  $("#time_start").change(function() {
-    if ($("#time_end").val()) { // Only update when second input has a value.
-      var duration = ($("#time_end").timepicker('getTime') - oldTime);
-      var newTime = $("#time_start").timepicker('getTime');
-      // Calculate and update the time in the second input.
-      $("#time_end").timepicker('setTime', new Date(newTime.getTime() + duration));
-      $('#time_end').effect('highlight', {}, 3000);
-      oldTime = newTime;
-    }
-  });
+});
 
-  // Update "date_end" so that it is the same as "date_start" if "date_start" is changed to after "date_end", and displays a highlight to alert the user.
-  $("#date_start").change(function() {
-    // Only update when end value is defined.
-    if ($("#date_end").val()) {
-      // Only update when end value is before start value.
-      var startDate = $.datepicker.parseDate('yy-mm-dd', $('#date_start').val());
-      var endDate = $.datepicker.parseDate('yy-mm-dd', $('#date_end').val());
-      if (endDate < startDate) {
-        $('#date_end').val($('#date_start').val());
-        $('#date_end').effect('highlight', {}, 3000);
-      }
+// Update "time_end" to maintain the same offset from "time_start" if "time_start" changes, and display a highlight to alert the user.
+$(document).on('change', '#time_start', function() {
+  if ($("#time_end").val()) { // Only update when second input has a value.
+    var duration = ($("#time_end").timepicker('getTime') - oldTime);
+    var newTime = $("#time_start").timepicker('getTime');
+    // Calculate and update the time in the second input.
+    $("#time_end").timepicker('setTime', new Date(newTime.getTime() + duration));
+    $('#time_end').effect('highlight', {}, 3000);
+    oldTime = newTime;
+  }
+});
+
+// Update "date_end" so that it is the same as "date_start" if "date_start" is changed to after "date_end", and displays a highlight to alert the user.
+$(document).on('change', '#date_start, #date_end',function() {
+  // Only update when end value is defined.
+  if ($("#date_end").val()) {
+    // Only update when end value is before start value.
+    var startDate = $.datepicker.parseDate('yy-mm-dd', $('#date_start').val());
+    var endDate = $.datepicker.parseDate('yy-mm-dd', $('#date_end').val());
+    if (endDate < startDate) {
+      $('#date_end').val($('#date_start').val());
+      $('#date_end').effect('highlight', {}, 3000);
     }
-  });
+  }
 });
