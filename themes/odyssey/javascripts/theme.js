7189290a8b1422e521b214edd7e5985fcaf0e848
itsderick <derickiscool@gmail.com>
2015-06-19 23:48:03 -0500

    Adding the search functionality for desktop and tablet. Changing the styles a bit for the calendar.

--- themes/odyssey/javascripts/theme.js
+++ themes/odyssey/javascripts/theme.js
@@ -1,1 +1,16 @@
 /* Nothing here yet */
+
+
+(function (window, document, undefined) {
+    window.onload = function windowLoad() {
+        console.log("The window loaded.");
+
+        var searchButton = document.querySelector('.js-search-box');
+        var searchForm = document.querySelector('.js-search-form');
+        var searchInput = document.querySelector('.js-search-input');
+        searchButton.addEventListener('click', function () {
+            searchButton.classList.toggle('is-open');
+            searchForm.classList.toggle('is-open');
+        });
+    };
+}(window, document));
