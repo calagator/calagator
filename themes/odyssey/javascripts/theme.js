06a301208588549ecc963a7e1ab095a9e15cd984
itsderick <derickiscool@gmail.com>
2015-06-22 20:34:19 -0500

    Making changes to the padding of the buttons, the color and padding of the navigation, fixing some issues with search not working on safari, making sure animations and transforms will work accross all browsers, fixing the margin and padding issue for the content in the individual events page, putting next and previous month buttons inside of the month bar, and finally styling the subscribe box in the events search page to be at the bottom.

--- themes/odyssey/javascripts/theme.js
+++ themes/odyssey/javascripts/theme.js
@@ -3,14 +3,20 @@
 
 (function (window, document, undefined) {
     window.onload = function windowLoad() {
-        console.log("The window loaded.");
-
         var searchButton = document.querySelector('.js-search-box');
         var searchForm = document.querySelector('.js-search-form');
         var searchInput = document.querySelector('.js-search-input');
         searchButton.addEventListener('click', function () {
             searchButton.classList.toggle('is-open');
             searchForm.classList.toggle('is-open');
+
+            if (searchForm.classList.contains('is-open')) {
+                setTimeout(function () {
+                    searchInput.focus();
+                }, 250);
+            } else {
+                searchInput.blur();
+            }
         });
     };
 }(window, document));
