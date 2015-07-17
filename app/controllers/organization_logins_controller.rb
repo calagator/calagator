d324b7608eb2350d0b40e03f01d6ebd5b0ec12ff
Josh W Lewis <josh.w.lewis@gmail.com>
2015-07-17 11:11:25 -0500

    Redirect organization users to the root path when logging in.

--- app/controllers/organization_logins_controller.rb
+++ app/controllers/organization_logins_controller.rb
@@ -2,7 +2,7 @@ class OrganizationLoginsController < ApplicationController
   def create
     if org = Organization.find_by_permalink(params[:permalink])
       session[:organization_id] = org.id
-      redirect_to events_path, flash: { success: 'Logged in.' }
+      redirect_to root_path, flash: { success: 'Logged in.' }
     else
       render :file => "#{Rails.root}/public/404.html",  :status => 404
     end
