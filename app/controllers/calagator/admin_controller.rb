ba9fa52d2684fb588d922da12f6fb9dfc74459db
Shawna C. Scott <shawnatyger@gmail.com>
2015-08-07 17:49:08 -0700

    Make admin lock event have search

--- app/controllers/calagator/admin_controller.rb
+++ app/controllers/calagator/admin_controller.rb
@@ -1,27 +1,40 @@
 module Calagator
 
-class AdminController < Calagator::ApplicationController
-  require_admin
+  class AdminController < Calagator::ApplicationController
+    require_admin
 
-  def index
-  end
-
-  def events
-    @events = Event.future
-  end
-
-  def lock_event
-    @event = Event.find(params[:event_id])
-    if @event.locked?
-      @event.unlock_editing!
-      flash[:success] = "Unlocked event #{@event.title} (#{@event.id})"
-    else
-      @event.lock_editing!
-      flash[:success] = "Locked event #{@event.title} (#{@event.id})"
+    def index
     end
-    redirect_to :action => :events
-  end
 
-end
+    def events
+      if params[:query].blank?
+        @events = Event.future
+      else
+        @search = Event::Search.new(params)
+        @admin_query = params[:query]
+
+        @events = @search.events
+
+        flash[:failure] = @search.failure_message
+        return redirect_to admin_events_path if @search.hard_failure?
+      end
+
+      render "calagator/admin/events"
+    end
+
+    def lock_event
+      @event = Event.find(params[:event_id])
+
+      if @event.locked?
+        @event.unlock_editing!
+        flash[:success] = "Unlocked event #{@event.title} (#{@event.id})"
+      else
+        @event.lock_editing!
+        flash[:success] = "Locked event #{@event.title} (#{@event.id})"
+      end
+      redirect_to :action => :events, :query => params[:query]
+    end
+
+  end
 
 end
