128bf5fb3fdef22969816b21b3b121266a87e44b
Micah Geisel <micah@botandrose.com>
2015-09-09 13:35:00 -0700

    extract MachineTags configuration to initializer.

--- lib/calagator/machine_tag.rb
+++ lib/calagator/machine_tag.rb
@@ -8,53 +8,9 @@ class MachineTag < Struct.new(:name)
     end
   end
 
-  # Structure of machine tag namespaces and predicates to their URLs. See
-  # #machine_tag for details.
-  MACHINE_TAG_URLS = {
-    'epdx' => {
-      'company' => 'http://epdx.org/companies/%s',
-      'group' => 'http://epdx.org/groups/%s',
-      'person' => 'http://epdx.org/people/%s',
-      'project' => 'http://epdx.org/projects/%s',
-    },
-    'upcoming' => {
-      'event' => "http://upcoming.yahoo.com/event/%s",
-      'venue' => "http://upcoming.yahoo.com/venue/%s"
-    },
-      'plancast' => {
-      'activity' => "http://plancast.com/a/%s",
-      'plan' => "http://plancast.com/p/%s"
-    },
-      'yelp' => {
-      'biz' => "http://www.yelp.com/biz/%s"
-    },
-      'foursquare' => {
-      'venue' => "http://foursquare.com/venue/%s"
-    },
-      'gowalla' => {
-      'spot' => "http://gowalla.com/spots/%s"
-    },
-      'shizzow' => {
-      'place' => "http://www.shizzow.com/places/%s"
-    },
-      'meetup' => {
-      'group' => "http://www.meetup.com/%s"
-    },
-      'facebook' => {
-      'event' => "http://www.facebook.com/event.php?eid=%s"
-    },
-      'lanyrd' => {
-      'event' => "http://lanyrd.com/%s"
-    }
-  } unless defined?(MACHINE_TAG_URLS)
-
-  # Regular expression for parsing machine tags
-  MACHINE_TAG_PATTERN = /(?<namespace>[^:]+):(?<predicate>[^=]+)=(?<value>.+)/ unless defined?(MACHINE_TAG_PATTERN)
-
-  # Machine tag predicates that refer to place entries
-  VENUE_PREDICATES = %w(venue place spot biz) unless defined?(VENUE_PREDICATES)
-
-  DEFUNCT_NAMESPACES = %w(upcoming gowalla shizzow)
+  cattr_accessor(:urls) { Hash.new }
+  cattr_accessor(:venue_predicates) { Array.new }
+  cattr_accessor(:defunct_namespaces) { Array.new }
 
   # Return a machine tag hash for this tag, or an empty hash if this isn't a
   # machine tag. The hash will always contain :namespace, :predicate and :value
@@ -82,10 +38,13 @@ class MachineTag < Struct.new(:name)
   end
 
   def venue?
-    VENUE_PREDICATES.include? predicate
+    venue_predicates.include? predicate
   end
 
   private
+
+  # Regular expression for parsing machine tags
+  MACHINE_TAG_PATTERN = /(?<namespace>[^:]+):(?<predicate>[^=]+)=(?<value>.+)/
 
   def matches
     name.match(MACHINE_TAG_PATTERN)
@@ -104,7 +63,7 @@ class MachineTag < Struct.new(:name)
   end
 
   def url
-    return unless machine_tag = MACHINE_TAG_URLS[namespace]
+    return unless machine_tag = urls[namespace]
     return unless url_template = machine_tag[predicate]
     url = sprintf(url_template, value)
     url = "#{site_root_url}defunct?url=https://web.archive.org/web/#{archive_date}/#{url}" if defunct?
@@ -112,7 +71,7 @@ class MachineTag < Struct.new(:name)
   end
 
   def defunct?
-    DEFUNCT_NAMESPACES.include? namespace
+    defunct_namespaces.include? namespace
   end
 
   def archive_date
