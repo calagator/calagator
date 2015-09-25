f5e39b693ef7dff74feb571ea54cfcb84dfa0124
Micah Geisel <micah@botandrose.com>
2015-09-25 15:51:31 -0700

    spec should use public API.

--- spec/models/calagator/source/parser_ical_non_standard_spec.rb
+++ spec/models/calagator/source/parser_ical_non_standard_spec.rb
@@ -42,40 +42,21 @@ ICAL
   it "should have the locality as is" do
     expect(subject.locality).to eq 'Portland'
   end
-end
-
-describe Source::Parser::Ical::VVenue, "when parsing VCARD lines", :type => :model do
-   before(:each) do
-     # Note that each line here represents a single, complete property definition -- this method doesn't do any magical unwrapping of text.
-     @vcard_hash = described_class.new.send :hash_from_vcard_lines, %(
-X-VVENUE-INFO:http://evdb.com/docs/ical-venue/draft-norris-ical-venue.html
-UID:V0-001-001423875-1@eventful.com
-NAME:Apple Store Pioneer Place
-DESCRIPTION:(503) 222-3002 Driving Directions & Map  Store Hours:  Mon - Fri: 9:30 a.m. to 9:00 p.m. Sat: 9:30 a.m. to 8:00 p.m. Sun: 11:00 a.m. to 6:00 p.m.
-ADDRESS:700 Southwest Fifth Avenue Suite; #1035
-CITY:Portland
-REGION;KMeta=none&bizzare:Oregon
-COUNTRY;;;ABBREV=USA:United States
-POSTALCODE:97204
-GEO:45.518798;-122.677583
-URL;X-LABEL=Venue Info:http://eventful.com/V0-001-001423875-1
-CATEGORIES:apple applecom appleinc technology).split("\n")
-  end
 
   it "should find a property set by its key" do
-    expect(@vcard_hash['NAME']).to eq 'Apple Store Pioneer Place'
+    expect(vvenue.vcard_hash['NAME']).to eq 'Apple Store Pioneer Place'
   end
 
   it "should find a property set by its key and meta-qualifier by its key when one wasn't specified" do
-    expect(@vcard_hash['URL']).to eq 'http://eventful.com/V0-001-001423875-1'
+    expect(vvenue.vcard_hash['URL']).to eq 'http://eventful.com/V0-001-001423875-1'
   end
 
   it "should find a property set by its key and multiple meta-qualifiers by its key when one wasn't specified" do
-    expect(@vcard_hash['COUNTRY']).to eq 'United States'
+    expect(vvenue.vcard_hash['COUNTRY']).to eq 'United States'
   end
 
   it "should find a property set by its key and meta-qualifier with odd characters by its key when one wasn't specified" do
-    expect(@vcard_hash['REGION']).to eq 'Oregon'
+    expect(vvenue.vcard_hash['REGION']).to eq 'Oregon'
   end
 end
 
