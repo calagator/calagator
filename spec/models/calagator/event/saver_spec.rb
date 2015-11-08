5cf3f08e06cbe2ac548c4ba544b4004c31591497
supremebeing7 <markopolo@gmail.com>
2015-11-07 20:36:51 -0800

    add additional Event::Saver specs

--- spec/models/calagator/event/saver_spec.rb
+++ spec/models/calagator/event/saver_spec.rb
@@ -13,6 +13,29 @@ module Calagator
         expect(saver.failure).to be_nil
       end
 
+      it "should fail to save an event with too many links" do
+        event.description << "\n\nhttp://google.com"
+        event.description << "\n\nhttp://example.com"
+        event.description << "\n\nhttp://calagator.com"
+        event.description << "\n\nhttp://disallowed.link"
+        saver = Event::Saver.new(event, params)
+        saver.save
+        expect(saver.failure).to include "too many links"
+      end
+
+      it "should fail to save an event made by an evil robot" do
+        params[:trap_field] = "It's a trap!"
+        saver = Event::Saver.new(event, params)
+        saver.save
+        expect(saver.failure).to include "you're an evil robot"
+      end
+
+      it "should not save an event being previewed" do
+        params[:preview] = "Preview"
+        saver = Event::Saver.new(event, params)
+        expect(saver.save).to be false
+      end
+
       it "should save a valid imported event with more than 3 links" do
         saver = Event::Saver.new(imported_event, params)
         saver.save
