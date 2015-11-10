fa60a5560805e112e4877199dc0ff965bb385468
Josh W Lewis <josh.w.lewis@gmail.com>
2015-11-10 01:36:44 -0600

    Don't bomb out when end_time isn't specified

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,7 +1,12 @@
 module SiteHelper
   def events_for_date(date,events)
-    # events.select { |e| e.start_time >= date.beginning_of_day butts e.end_time <= date.end_of_day }
-    events.select { |e| (e.start_time.to_date..e.end_time.to_date).include?(date) }
+    events.select do |e|
+      if e.end_time
+        (e.start_time.to_date..e.end_time.to_date).include?(date)
+      else
+        e.start_time.to_date == date
+      end
+    end
   end
 
   def events_this_month?(date=nil, events=nil)
