186128fcf817143ef4670509146b77570b0330fb
Reid Beels <mail@reidbeels.com>
2020-03-14 12:26:34 -0700

    Monkeypatch ActionController::TestResponse to allow tests to run on Ruby 2.6+

---
+++ spec/support/rails_4_2_ruby_2_6.rb
@@ -0,0 +1,17 @@
+# Allow tests to run on for Rails 4.2 on Ruby 2.6+
+# Via: https://github.com/rails/rails/issues/34790#issuecomment-450502805
+
+if RUBY_VERSION>='2.6.0'
+  if Rails.version < '5'
+    class ActionController::TestResponse < ActionDispatch::TestResponse
+      def recycle!
+        # hack to avoid MonitorMixin double-initialize error:
+        @mon_mutex_owner_object_id = nil
+        @mon_mutex = nil
+        initialize
+      end
+    end
+  else
+    puts "Monkeypatch for ActionController::TestResponse no longer needed"
+  end
+end
