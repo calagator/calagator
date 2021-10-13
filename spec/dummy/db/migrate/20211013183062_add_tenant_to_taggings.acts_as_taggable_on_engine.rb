93f28d3eccc2f18b33c39c58a85a1c4a3c153d6e
Audrey Eschright <audrey@lifeofaudrey.com>
2021-10-13 14:13:12 -0700

    Add test app migrations

---
+++ spec/dummy/db/migrate/20211013183062_add_tenant_to_taggings.acts_as_taggable_on_engine.rb
@@ -0,0 +1,17 @@
+# This migration comes from acts_as_taggable_on_engine (originally 7)
+if ActiveRecord.gem_version >= Gem::Version.new('5.0')
+  class AddTenantToTaggings < ActiveRecord::Migration[4.2]; end
+else
+  class AddTenantToTaggings < ActiveRecord::Migration; end
+end
+AddTenantToTaggings.class_eval do
+  def self.up
+    add_column :taggings, :tenant, :string, limit: 128
+    add_index :taggings, :tenant unless index_exists? :taggings, :tenant
+  end
+
+  def self.down
+    remove_index :taggings, :tenant
+    remove_column :taggings, :tenant
+  end
+end
