ad48ed0ad2fdbcb694f5703a26ea70d6580297bd
Audrey Eschright <audrey@lifeofaudrey.com>
2021-11-05 16:04:46 -0700

    Update app/models/calagator/application_record.rb
    Co-authored-by: Reid Beels <mail@reidbeels.com>

--- app/models/calagator/application_record.rb
+++ app/models/calagator/application_record.rb
@@ -1,3 +1,5 @@
-class ApplicationRecord < ActiveRecord::Base
-  self.abstract_class = true
+module Calagator
+  class ApplicationRecord < ActiveRecord::Base
+    self.abstract_class = true
+  end
 end
