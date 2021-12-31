83f9120ddc72da58910b2e39c8a0fce7eb2c9e3b
Audrey Eschright <audrey@lifeofaudrey.com>
2021-12-30 16:15:28 -0800

    Set time zone for test app

--- spec/test_app/config/application.rb
+++ spec/test_app/config/application.rb
@@ -16,7 +16,7 @@ module TestApp
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Pacific Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
