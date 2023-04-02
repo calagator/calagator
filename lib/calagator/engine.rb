2e5509b8669111de8371b693e08f8dcc21abe1a6
Ryan W <rwilliamz@gmail.com>
2023-04-01 20:52:14 -0700

    Check if method exists

--- lib/calagator/engine.rb
+++ lib/calagator/engine.rb
@@ -19,7 +19,9 @@ module Calagator
       leaflet
     ]
 
-    config.active_record.yaml_column_permitted_classes = [Symbol, Date, Time]
+    if config.active_record.respond_to?(:yaml_column_permitted_classes)
+      config.active_record.yaml_column_permitted_classes = [Symbol, Date, Time]
+    end
 
     config.after_initialize do
       Calagator.configure_search_engine
