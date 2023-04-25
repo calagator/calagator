5778fc731209b5abcfa27a12cd4e2e40f228b394
Richie Khoo <evolve2k@gmail.com>
2023-04-25 16:41:34 +0930

    Lint/UselessAssignment: Useless assignment to variable - `error` — Removed assignment.

--- app/models/calagator/source/parser.rb
+++ app/models/calagator/source/parser.rb
@@ -106,8 +106,7 @@ module Calagator
       data = JSON.parse(data)
 
       # Stop if API tells us there's an error.
-      raise Source::Parser::NotFound, error if (error = data[error_key])
-
+      raise Source::Parser::NotFound, error if data[error_key]
       data["event_id"] = event_id
       data
     end
