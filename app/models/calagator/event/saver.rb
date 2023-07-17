cc4fde4814f0396257a170c40d4b72ea24bec902
Devin Gaffney <itsme@devingaffney.com>
2023-07-16 19:47:17 -0700

    Update saver.rb
    Flip label - 1 is legit, 0 is spam

--- app/models/calagator/event/saver.rb
+++ app/models/calagator/event/saver.rb
@@ -42,7 +42,7 @@ module Calagator
       end
 
       def ml_flags_as_spam?
-        if get_spam_label == 1
+        if get_spam_label != 1
           self.failure = "<h3>Spammer</h3> We didn't save this event because we think this looks like a spammy event. If this isn't spam and is a legitimate event, please file a bug report and let us know."
         end
       end
