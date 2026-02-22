333561252be182692a92d00708f81eaed0930172
Reid Beels <mail@reidbeels.com>
2026-02-21 20:31:25 -0800

    Regenerate schema.rb for Rails 7.1

--- spec/test_app/db/schema.rb
+++ spec/test_app/db/schema.rb
@@ -2,26 +2,26 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_040656) do
+ActiveRecord::Schema[7.1].define(version: 2020_03_27_040656) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "start_time"
+    t.datetime "start_time", precision: nil
     t.integer "venue_id"
     t.string "url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.integer "source_id"
     t.integer "duplicate_of_id"
-    t.datetime "end_time"
+    t.datetime "end_time", precision: nil
     t.string "rrule"
     t.text "venue_details"
     t.boolean "locked", default: false
@@ -30,9 +30,9 @@ ActiveRecord::Schema.define(version: 2020_03_27_040656) do
   create_table "sources", force: :cascade do |t|
     t.string "title"
     t.string "url"
-    t.datetime "imported_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "imported_at", precision: nil
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
   end
 
   create_table "taggings", force: :cascade do |t|
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2020_03_27_040656) do
     t.integer "tagger_id"
     t.string "tagger_type"
     t.string "context"
-    t.datetime "created_at"
+    t.datetime "created_at", precision: nil
     t.index ["context"], name: "index_taggings_on_context"
     t.index ["tag_id"], name: "index_taggings_on_tag_id"
     t.index ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
@@ -64,8 +64,8 @@ ActiveRecord::Schema.define(version: 2020_03_27_040656) do
     t.text "description"
     t.string "address"
     t.string "url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.string "street_address"
     t.string "locality"
     t.string "region"
@@ -89,7 +89,7 @@ ActiveRecord::Schema.define(version: 2020_03_27_040656) do
     t.string "event", null: false
     t.string "whodunnit"
     t.text "object"
-    t.datetime "created_at"
+    t.datetime "created_at", precision: nil
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 end
